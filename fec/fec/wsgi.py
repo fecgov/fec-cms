8dbfdff9cff691003ba5303c084bf06d776dced0
Joshua Carp <jm.carp@gmail.com>
2015-10-14 16:17:15 -0400

    Verify HMAC signatures.

--- fec/fec/wsgi.py
+++ fec/fec/wsgi.py
@@ -10,8 +10,22 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fec.settings.production')
 
+import hashlib
+
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+from hmac_authentication import hmacauth
+
+from fec.settings.credentials import credentials
 
 application = get_wsgi_application()
+
 application = DjangoWhiteNoise(application)
+
+auth = hmacauth.HmacAuth(
+    digest=hashlib.sha1,
+    secret_key=credentials.get('HMAC_SECRET'),
+    signature_header='X-Signature',
+    headers=credentials.get('HMAC_HEADERS', '').split(','),
+)
+application = hmacauth.HmacMiddleware(application, auth)
