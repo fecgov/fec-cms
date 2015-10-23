7323bb8254aa3f316a978ee28acfcbe314243b6e
Joshua Carp <jm.carp@gmail.com>
2015-10-23 11:14:53 -0400

    Add feedback widget to base template.
    * Add feedback widget to fec.js
    * Install and apply stringify transform
    * Add app URL globally to base template

--- fec/gulpfile.js
+++ fec/gulpfile.js
@@ -3,11 +3,13 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var stringify = require('stringify');
 
 var entry = './fec/static/js/fec.js';
 
 gulp.task('build-js', function () {
   return browserify(entry)
+    .transform({global: true}, stringify(['.html']))
     .bundle()
     .pipe(source(entry))
     .pipe(gulp.dest('dist'));
