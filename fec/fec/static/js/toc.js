7e9a1da820ff85a7fe995658851dd121ea194b67
Noah Manger <noah.manger@gsa.gov>
2015-12-09 17:36:03 -0800

    Sticky side nav
    - Implements the component-sticky library to sticky the side nav
    - Adds a module based on scroll monitor to highlight active elements of
    a table of contents
    - Implements on reg. & reporting landing page and checklist page

---
+++ fec/fec/static/js/toc.js
@@ -0,0 +1,68 @@
+'use strict';
+
+/* global require, module */
+
+var $ = require('jquery');
+var scrollMonitor = require('scrollmonitor');
+
+/**
+ * Table of Contents widget
+ *
+ * 1. Takes a list of links and finds all sections with IDs matching their hrefs
+ * 2. Adds scrollwatchers to highlight the menu item when the section reaches top of viewport
+ * 3. Animates the document to scroll to the section when clicking the link
+ *
+ * @constructor
+ * @param {string} selector - Selector for the navigation menu for the TOC
+ */
+
+function TOC(selector) {
+  this.$menu = $(selector);
+  this.sections = this.getSections();
+  this.offset = (-1 * scrollMonitor.viewportHeight);
+  this.addWatchers();
+  this.$menu.on('click', 'a', this.scrollTo.bind(this));
+}
+
+TOC.prototype.getSections = function() {
+  var sections = [];
+  this.$menu.find('a').each(function() {
+    var section = $(this).attr('href');
+    sections.push(section);
+  });
+
+  return sections;
+};
+
+TOC.prototype.addWatchers = function() {
+  var self = this;
+
+  this.sections.forEach(function(section) {
+    var elm = document.querySelector(section);
+    var watcher = scrollMonitor.create(elm, {top: self.offset});
+    watcher.$menuItem = self.$menu.find('a[href=' + section + ']');
+    watcher.enterViewport(function() {
+      self.highlightActiveItem(this);
+    });
+  });
+};
+
+TOC.prototype.highlightActiveItem = function(watcher) {
+  var $currentHighlight = this.$menu.find('a.is-active');
+  if (watcher.isInViewport) {
+    $currentHighlight.removeClass('is-active');
+    watcher.$menuItem.addClass('is-active');
+  }
+};
+
+TOC.prototype.scrollTo = function(e) {
+  e.preventDefault();
+  var $link = $(e.target);
+  var section = $link.attr('href');
+  var sectionTop = $(section).offset().top;
+  $(document.body).animate({
+    scrollTop: sectionTop
+  });
+};
+
+module.exports = { TOC: TOC };
