7847620341f43900718fb8c031729197b3e91e99
Joshua Carp <jm.carp@gmail.com>
2015-12-21 12:06:18 -0500

    Add source maps in debug mode.

--- fec/gulpfile.js
+++ fec/gulpfile.js
@@ -1,12 +1,15 @@
+/* global process */
+
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var stringify = require('stringify');
 
 var entry = './fec/static/js/fec.js';
+var debug = !!process.env.FEC_CMS_DEBUG;
 
 gulp.task('build-js', function () {
-  return browserify(entry)
+  return browserify(entry, {debug: debug})
     .transform({global: true}, stringify(['.html']))
     .bundle()
     .pipe(source(entry))
