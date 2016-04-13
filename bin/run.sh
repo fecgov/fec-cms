a16cdf06dd18cf1d882284d3bb02191289a55da9
Carlo Costino <carlo.costino@gsa.gov>
2016-04-13 10:11:57 -0400

    Added missing shebang line and a couple of options based on review feedback
    h/t @adborden

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# Set environment options to exit immediately if a non-zero status code
+# appears from a command or within a pipe
+set -o errexit
+set -o pipefail
+
+# Send out Slack notification(s)
 invoke notify
 
 # Build static files
