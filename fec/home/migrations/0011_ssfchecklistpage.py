0da52ddc6a938f64a5e6aa7d98c63fd8db6ff3bc
Noah Manger <noah.manger@gsa.gov>
2016-04-27 14:59:19 -0700

    Adding checklist and landing page content for party committees

--- fec/home/migrations/0011_ssfchecklistpage.py
+++ fec/home/migrations/0011_ssfchecklistpage.py
@@ -26,4 +26,15 @@ class Migration(migrations.Migration):
             },
             bases=('wagtailcore.page',),
         ),
+        migrations.CreateModel(
+            name='PartyChecklistPage',
+            fields=[
+                ('page_ptr', models.OneToOneField(primary_key=True, auto_created=True, to='wagtailcore.Page', serialize=False, parent_link=True)),
+                ('body', wagtail.wagtailcore.fields.StreamField((('heading', wagtail.wagtailcore.blocks.CharBlock(classname='full title')), ('paragraph', wagtail.wagtailcore.blocks.RichTextBlock()), ('html', wagtail.wagtailcore.blocks.RawHTMLBlock()), ('image', wagtail.wagtailimages.blocks.ImageChooserBlock())), blank=True, null=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('wagtailcore.page',),
+        ),
     ]
