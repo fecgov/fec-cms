f2cf1057d5910356895439a1c4343458d16729de
Joshua Carp <jm.carp@gmail.com>
2016-05-07 16:24:48 -0400

    Build assets on CI.
    * Switch to vetted python buildpack
    * Build assets on travis and include in deploy
    * Remove `dist` from `.cfignore`
    
    Part of https://github.com/18F/openFEC/issues/1643.

--- bin/run.sh
+++ bin/run.sh
@@ -10,7 +10,6 @@ invoke notify
 
 # Build static files
 cd fec
-gulp build-js
 ./manage.py collectstatic --settings fec.settings.production --noinput
 ./manage.py compress --settings fec.settings.production
 
