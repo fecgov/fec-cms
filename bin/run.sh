ac9997eb668d8825d7e6b143cf00723d6ae4bf36
Joshua Carp <jm.carp@gmail.com>
2016-05-24 11:55:46 -0400

    Skip collectstatic.
    h/t @adborden

--- bin/run.sh
+++ bin/run.sh
@@ -10,12 +10,11 @@ invoke notify
 
 # Build static files
 cd fec
-./manage.py collectstatic --settings fec.settings.production --noinput
-./manage.py compress --settings fec.settings.production
+./manage.py compress
 
 # Run migrations
-./manage.py makemigrations --settings fec.settings.production
-./manage.py migrate --settings fec.settings.production --noinput
+./manage.py makemigrations
+./manage.py migrate --noinput
 
 # Run application
 gunicorn fec.wsgi:application
