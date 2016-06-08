ea79c1654ea2e2fc82fa559e3f480c385e0eed7b
Noah Manger <noah.manger@gsa.gov>
2016-06-08 13:53:28 -0700

    Revert "Revert "Merge branch 'master' of https://github.com/18F/fec-cms""
    This reverts commit f542b784eda2483fce45e6feb76c426ee6da13a1.
    
    Conflicts:
    	package.json

--- fec/fec/static/js/fec.js
+++ fec/fec/static/js/fec.js
@@ -12,6 +12,8 @@ var siteNav = require('fec-style/js/site-nav');
 var dropdown = require('fec-style/js/dropdowns');
 var FilterPanel = require('fec-style/js/filter-panel').FilterPanel;
 var filterTags = require('fec-style/js/filter-tags');
+var stickyBar = require('fec-style/js/sticky-bar');
+var toc = require('fec-style/js/toc');
 
 // Hack: Append jQuery to `window` for use by legacy libraries
 window.$ = window.jQuery = $;
@@ -19,7 +21,8 @@ window.$ = window.jQuery = $;
 var Sticky = require('component-sticky');
 var calendar = require('./calendar');
 var calendarHelpers = require('./calendar-helpers');
-var toc = require('./toc');
+
+var legal = require('./legal');
 
 $(document).ready(function() {
   // Initialize glossary
@@ -54,12 +57,17 @@ $(document).ready(function() {
   new toc.TOC('.js-toc');
 
   // Initialize sticky elements
-  $('.js-sticky').each(function() {
+  $('.js-sticky-side').each(function() {
     var container = $(this).data('sticky-container');
     var opts = {
       within: document.getElementById(container)
     };
     new Sticky(this, opts);
+  });
+
+  // Initialize sticky bar elements
+  $('.js-sticky-bar').each(function() {
+    new stickyBar.StickyBar(this);
   });
 
   // Initialize checkbox dropdowns
@@ -68,7 +76,10 @@ $(document).ready(function() {
   });
 
   // Initialize feedback widget
-  new feedback.Feedback(window.FEC_APP_URL + '/issue/');
+  var feedbackWidget = new feedback.Feedback(window.FEC_APP_URL + '/issue/');
+
+  // Initialize legal page
+  new legal.Legal(feedbackWidget, '#share-feedback-link', '#ethnio-link');
 
   // Initialize filter tags
   var $tagList = new filterTags.TagList({title: 'All records'}).$body;
