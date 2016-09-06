b539b69b0c69e18021b097e2f158d95348a04d0f
Noah Manger <noah.manger@gsa.gov>
2016-09-06 16:20:11 -0700

    Updating update view and template
    - Query update types and categories based on request params
    - Combine all update types into a single list
    - Add pagination
    - Add basic navigation and categories

--- fec/home/views.py
+++ fec/home/views.py
@@ -1,15 +1,54 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from itertools import chain
 from home.models import DigestPage
 from home.models import RecordPage
 from home.models import PressReleasePage
 
 def updates(request):
-    digests = DigestPage.objects.all()
-    records = RecordPage.objects.all()
-    press_releases = PressReleasePage.objects.all()
+    update_types = request.GET.getlist('update_type', None)
+    categories = request.GET.getlist('category', None)
+    digests = ''
+    records = ''
+    press_releases = ''
+
+    # If there's a query, only get the types in the query
+    if update_types:
+      if 'record' in update_types:
+        records = RecordPage.objects.all()
+      if 'digests' in update_types:
+        digests = DigestPage.objects.all()
+      if 'press_release' in update_types:
+        press_releases = PressReleasePage.objects.all()
+    else:
+      records = RecordPage.objects.all()
+      digests = DigestPage.objects.all()
+      press_releases = PressReleasePage.objects.all()
+
+    # Chain all the QuerySets together
+    # via http://stackoverflow.com/a/434755/1864981
+    updates = list(chain(digests, records, press_releases))
+
+    # Filter out any that don't match the category
+    if categories:
+      for update in updates:
+        category = update.category.replace(' ', '-')
+        if category not in categories:
+          updates.remove(update)
+
+    # Handle pagination
+    page = request.GET.get('page', 1)
+    paginator = Paginator(updates, 20)
+    try:
+        updates = paginator.page(page)
+    except PageNotAnInteger:
+        updates = paginator.page(1)
+    except EmptyPage:
+        updates = paginator.page(paginator.num_pages)
+
 
     return render(request, 'home/latest_updates.html', {
-        'digests': digests,
-        'records': records,
-        'press_releases' : press_releases,
+        'categories': categories,
+        'update_types': update_types,
+        'updates': updates,
     })
