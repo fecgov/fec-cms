f15f247ab731dc70f148e249b82a07d891346221
LindsayYoung <lindsayn.young@gsa.gov>
2016-10-12 17:17:42 -0400

    It is not saving correctly but the regex is working.

--- fec/home/utils/link_reroute.py
+++ fec/home/utils/link_reroute.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
 
 
-bucket_url = 'https://amazon-ugly-bucketname'
+bucket_url = 'https://cg-26646295-a781-431c-ab40-895616b7ea28'
 
 
 def new_press_link(l):
@@ -28,22 +28,22 @@ def make_absolute_links(orig_href, body):
     return str(soup)
 
 # this can be the function the remake all the links
-def remake_links(href, body):
+def remake_links(body):
     # find relative links
     soup = bs(body, "html5lib")
-    links = soup.find_all('a')
+    links = soup.find_all('a', href=True)
     for link in links:
-        # looking for all realitive links
-        if 'href' in link and link['href'][:7] != 'http://':
-            if link['href'][:1] == '/':
-                link['href'] = '/' + link['href']
-
-            # re-save links as absolute links
-            link['href'] = urljoin(href, link['href'])
-
         # we can add more links to check here
         path_replacements = [
+            # Press path replacements
+            ('http://fec.gov/press/press[0-9]+/news_releases/', new_press_link(link['href'])),
             ('http://fec.gov/press/press[0-9]+/', new_press_link(link['href'])),
+            ('http://fec.gov/press/archive/[0-9]+/archive/[0-9]+/', new_press_link(link['href'])),
+            ('http://fec.gov/press/archive/[0-9]+/', new_press_link(link['href'])),
+            ('http://www.fec.gov/press/press[0-9]+/news_releases/', new_press_link(link['href'])),
+            ('http://www.fec.gov/press/press[0-9]+/', new_press_link(link['href'])),
+            ('http://www.fec.gov/press/archive/[0-9]+/archive/[0-9]+/', new_press_link(link['href'])),
+            ('http://www.fec.gov/press/archive/[0-9]+/', new_press_link(link['href'])),
         ]
 
         for old, new in path_replacements:
@@ -51,9 +51,17 @@ def remake_links(href, body):
             if re.match(re_string, link['href']):
                 link['href'] = re.sub(old, new, link['href'])
 
-    return soup
+    return str(soup)
 
 # # using this to test
-# test = 'lsdkjfkj <a href="/test.com">x</a> sijdflkj<a href="http://www.example/test.com">y</a> asd <a href="http://fec.gov/press/press1999/testy.pdf">z</a>'
-
-# print(remake_links('http://fec.gov/', test))
+# test = """<html><head></head><body>
+#     lsdkjfkj <a href="/test.com">x</a> sijdflkj
+#     <a href="http://www.example/test.com">y</a> asd
+#     <a href="http://fec.gov/press/archive/1975/archive/1975/archive.pdf">archive releases 1</a>
+#     <a href="http://fec.gov/press/archive/1975/archive.pdf">archive releases 2</a>
+#     <a href="http://fec.gov/press/press2016/news_releases/current.pdf">current releases</a>
+#     <a href="http://fec.gov/press/press2010/2010_18M-Candidate_Files/1cansum201018.pdf">current releases type 2</a>'
+#     <a href="http://www.fec.gov/press/archive/1977/archive/1977/19770210_MatchingFunds.pdf">test</a>
+#     </body>
+# """
+# print(remake_links(test))
