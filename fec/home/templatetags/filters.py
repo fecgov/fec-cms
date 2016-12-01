a7dc60e1f9a7f1968e53f83f0713900ae22e158b
Noah Manger <noah.manger@gsa.gov>
2016-12-01 11:21:00 -0800

    Adding a model field and template tag for formatted titles

--- fec/home/templatetags/filters.py
+++ fec/home/templatetags/filters.py
@@ -1,6 +1,7 @@
 import re
 
 from django import template
+from django.utils.html import format_html
 
 register = template.Library()
 
@@ -11,3 +12,13 @@ def clean_whitespace(value):
 @register.filter
 def lookup(dict, arg):
   return dict.get(arg, '')
+
+@register.simple_tag()
+def formatted_title(page):
+  if hasattr(page, 'rich_title'):
+    if page.rich_title:
+      return format_html(page.rich_title)
+    else:
+      return format_html(page.title)
+  else:
+    return page.title
