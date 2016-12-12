711dc76f318dd9c0b523eede54078b2921001cf1
Pat Phongsvirajati <pphongsvirajati@fec.gov>
2016-12-12 12:27:13 -0500

    added typeahead and search js and initialized

--- fec/fec/static/js/fec.js
+++ fec/fec/static/js/fec.js
@@ -18,6 +18,8 @@ var FilterPanel = require('fec-style/js/filter-panel').FilterPanel;
 var filterTags = require('fec-style/js/filter-tags');
 var stickyBar = require('fec-style/js/sticky-bar');
 var toc = require('fec-style/js/toc');
+var Search = require('fec-style/js/search');
+var typeahead = require('fec-style/js/typeahead');
 
 // Hack: Append jQuery to `window` for use by legacy libraries
 window.$ = window.jQuery = $;
@@ -110,4 +112,10 @@ $(document).ready(function() {
     exportUrl: calendarHelpers.getUrl(['calendar-dates', 'export']),
     filterPanel: filterPanel,
   });
+
+  // Initialize search
+  new Search($('.js-search'));
+
+  // Initialize typeahead
+  new typeahead.Typeahead($('.js-search-input'));
 });
