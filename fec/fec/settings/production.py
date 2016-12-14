4227e45f557d138b0db970e3489240a685b492f3
Carlo Costino <carlo.costino@gsa.gov>
2016-12-13 21:56:03 -0500

    Remove redundant database setting

--- fec/fec/settings/production.py
+++ fec/fec/settings/production.py
@@ -3,9 +3,6 @@ import dj_database_url
 from .base import *  # noqa
 from .env import env
 
-
-DATABASES = {'default': dj_database_url.config()}
-
 SECRET_KEY = env.get_credential('DJANGO_SECRET_KEY')
 
 DEBUG = False
