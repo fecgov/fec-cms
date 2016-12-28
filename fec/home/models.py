f3fa4edc04b610de1621fae37312d63525c29a3c
Noah Manger <noah.manger@gsa.gov>
2016-12-28 15:05:24 -0800

    Adding a new ResourcePage template

--- fec/home/models.py
+++ fec/home/models.py
@@ -323,3 +323,93 @@ class CollectionPage(Page):
         StreamFieldPanel('related_pages'),
         StreamFieldPanel('sections'),
     ]
+
+class DocumentBlock(blocks.StructBlock):
+    """A document thumbnail in an aside or the main section"""
+    image = ImageChooserBlock()
+    url = blocks.URLBlock()
+    text = blocks.CharBlock()
+
+    class Meta:
+        icon = 'doc-empty'
+
+class AsideLinkBlock(blocks.StructBlock):
+    """Either a search or calendar link in a section aside"""
+    link_type = blocks.ChoiceBlock(choices=[
+        ('search', 'Search'),
+        ('calendar', 'Calendar')
+    ], icon='link', required=False, help_text='Set an icon')
+
+    url = blocks.URLBlock()
+    text = blocks.CharBlock(required=True)
+    coming_soon = blocks.BooleanBlock(required=False)
+
+    class Meta:
+        icon = 'link'
+
+class ContactItemBlock(blocks.StructBlock):
+    """A lockup of an icon and blurb of contact info"""
+    item_label = blocks.CharBlock(required=True)
+    item_icon = blocks.ChoiceBlock(choices=[
+        ('email', 'Email'),
+        ('fax', 'Fax'),
+        ('hand', 'Hand delivery'),
+        ('phone', 'Phone')
+    ], required=True)
+    item_info = blocks.RichTextBlock(required=True)
+
+    class Meta:
+        icon = 'cog'
+
+class ContactInfoBlock(blocks.StructBlock):
+    """ A StructBlock that can contain multiple contact items """
+    label = blocks.CharBlock(required=False, icon='title')
+    contact_items = blocks.ListBlock(ContactItemBlock())
+
+    class Meta:
+        template='blocks/contact-info.html'
+        icon='placeholder'
+
+class ResourceSection(blocks.StructBlock):
+    """A section of a ResourcePage"""
+    title = blocks.CharBlock(required=True)
+    hide_title = blocks.BooleanBlock(required=False, help_text='Should the section title be displayed?')
+    content = blocks.StreamBlock([
+        ('text', blocks.RichTextBlock(blank=False, null=False, required=False, icon='pilcrow')),
+        ('documents', blocks.ListBlock(DocumentBlock(), template='blocks/section-documents.html', icon='doc-empty')),
+        ('contact_info', ContactInfoBlock())
+    ])
+
+    aside = blocks.StreamBlock([
+        ('title', blocks.CharBlock(required=False, icon='title')),
+        ('document', DocumentBlock()),
+        ('link', AsideLinkBlock())
+    ],
+    template='blocks/section-aside.html',
+    icon='placeholder')
+
+class CitationsBlock(blocks.StructBlock):
+    """Block for a chunk of citations that includes a label and the citation (in content)"""
+    label = blocks.CharBlock()
+    content = blocks.RichTextBlock()
+
+class ResourcePage(Page):
+    """Class for pages that include a side nav, multiple sections and citations"""
+    intro = stream_factory(null=True, blank=True)
+    sections = StreamField([
+        ('sections', ResourceSection())
+    ], null=True)
+    citations = StreamField([
+        ('citations', blocks.ListBlock(CitationsBlock()))
+    ], null=True)
+    related_topics = StreamField([
+        ('related_topics', blocks.ListBlock(
+            blocks.PageChooserBlock(label="Related topic")
+        ))
+    ], null=True)
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('intro'),
+        StreamFieldPanel('sections'),
+        StreamFieldPanel('citations'),
+        StreamFieldPanel('related_topics')
+    ]
