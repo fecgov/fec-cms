639944446636512d4d465dc1b3f8381604a00b11
Noah Manger <noah.manger@gsa.gov>
2017-01-16 17:41:03 -0800

    Adding new content + record category filter

--- fec/fec/settings/base.py
+++ fec/fec/settings/base.py
@@ -186,7 +186,8 @@ CONSTANTS = constants
 
 FEATURES = {
     'record': bool(env.get_credential('FEC_FEATURE_RECORD', '')),
-    'about': bool(env.get_credential('FEC_FEATURE_ABOUT', ''))
+    'about': bool(env.get_credential('FEC_FEATURE_ABOUT', '')),
+    'agendas': bool(env.get_credential('FEC_FEATURE_AGENDAS', ''))
 }
 
 AUTH_PASSWORD_VALIDATORS = [
