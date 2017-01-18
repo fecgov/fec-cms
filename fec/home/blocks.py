9f1f6134bab556b01d08081456ff271d57e4173f
Noah Manger <noah.manger@gsa.gov>
2017-01-18 13:18:08 -0800

    Making some fields not required

--- fec/home/blocks.py
+++ fec/home/blocks.py
@@ -6,7 +6,7 @@ from wagtail.wagtaildocs.blocks import DocumentChooserBlock
 class ThumbnailBlock(blocks.StructBlock):
     """A block that combines a thumbnail and a caption,
         both of which link to a URL"""
-    image = ImageChooserBlock()
+    image = ImageChooserBlock(required=False)
     url = blocks.URLBlock()
     text = blocks.CharBlock()
 
@@ -29,7 +29,7 @@ class AsideLinkBlock(blocks.StructBlock):
 
 class ContactItemBlock(blocks.StructBlock):
     """A lockup of an icon and blurb of contact info"""
-    item_label = blocks.CharBlock(required=True)
+    item_label = blocks.CharBlock(required=False)
     item_icon = blocks.ChoiceBlock(choices=[
         ('email', 'Email'),
         ('fax', 'Fax'),
