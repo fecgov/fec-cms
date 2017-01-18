5d3effa9ed8c76d6f646842984e6ef81b32eecb7
Pat Phongsvirajati <pphongsvirajati@fec.gov>
2017-01-18 13:15:23 -0500

    added capability to upload documents and images from Wagtail to the s3 content bucket.

--- fec/fec/settings/base.py
+++ fec/fec/settings/base.py
@@ -210,3 +210,11 @@ if os.getenv('SENTRY_DSN'):
     RAVEN_CONFIG = {
         'dsn': os.getenv('SENTRY_DSN'),
     }
+
+AWS_QUERYSTRING_AUTH = False
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+MEDIA_URL = 'http://%s.s3-us-gov-west-1.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_LOCATION = 'cms-content'
