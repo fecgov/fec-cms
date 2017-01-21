d74a1d029c36c458c70f99214c286843d790555c
Noah Manger <noah.manger@gsa.gov>
2017-01-20 19:55:31 -0800

    Fixing committee id field

--- fec/fec/static/js/contact-form.js
+++ fec/fec/static/js/contact-form.js
@@ -5,7 +5,7 @@ var Typeahead = require('fec-style/js/typeahead').Typeahead;
 
 function ContactForm($elm) {
   this.$elm = $elm;
-  this.committee_id = $elm.find('id_u_committee');
+  this.committee_id = $elm.find('#id_u_committee');
   this.category = $elm.find('#id_u_category');
   this.otherReason = $elm.find('#id_u_other_reason').closest('div');
   this.typeahead = new Typeahead($elm.find('.js-contact-typeahead'), 'committees', '');
@@ -21,7 +21,7 @@ ContactForm.prototype.initTypeahead = function() {
   this.typeahead.$element.css({'height': 'auto'});
   this.typeahead.$input.off('typeahead:select');
   this.typeahead.$input.on('typeahead:select', function(e, opts) {
-    self.committee_id.value = opts.id;
+    self.committee_id.val(opts.id);
   });
 };
 
