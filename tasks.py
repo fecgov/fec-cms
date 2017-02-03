5b90d5d22e502908e8c6242db508ac07565af79e
Carlo Costino <carlo.costino@gsa.gov>
2017-02-03 14:36:53 -0500

    Add support for cloud.gov GovCloud migration
    This changeset includes the necessary changes required to migrate the FEC CMS app to the GovCloud cloud.gov environment from the old East/West environment.  Some of these changes may end up being unwound later on, but for the time being these are the pieces that are required in order to get the app running in the new environment.

--- tasks.py
+++ tasks.py
@@ -93,7 +93,7 @@ def deploy(ctx, space=None, branch=None, yes=False):
         return
 
     # Set api
-    api = 'https://api.cloud.gov'
+    api = 'https://api.fr.cloud.gov'
     ctx.run('cf api {0}'.format(api), echo=True)
 
     # Log in if necessary
@@ -101,7 +101,7 @@ def deploy(ctx, space=None, branch=None, yes=False):
         ctx.run('cf auth "$FEC_CF_USERNAME" "$FEC_CF_PASSWORD"', echo=True)
 
     # Target space
-    ctx.run('cf target -o fec -s {0}'.format(space), echo=True)
+    ctx.run('cf target -o fec-beta-fec -s {0}'.format(space), echo=True)
 
     # Set deploy variables
     with open('.cfmeta', 'w') as fp:
