f40d8bb96b0ef3e85ed41dbf40650f1f552d69aa
LindsayYoung <lindsayn.young@gsa.gov>
2017-03-07 14:30:19 -0500

    per-space deploy

--- tasks.py
+++ tasks.py
@@ -80,7 +80,7 @@ DEPLOY_RULES = (
 
 
 @task
-def deploy(ctx, space=None, branch=None, yes=False):
+def deploy(ctx, space=None, branch=None, login=None, yes=False):
     """Deploy app to Cloud Foundry. Log in using credentials stored in
     `FEC_CF_USERNAME` and `FEC_CF_PASSWORD`; push to either `space` or the space
     detected from the name and tags of the current branch. Note: Must pass `space`
@@ -111,8 +111,8 @@ def deploy(ctx, space=None, branch=None, yes=False):
     ctx.run('cf api {0}'.format(api), echo=True)
 
     # Log in if necessary
-    if os.getenv('FEC_CF_USERNAME') and os.getenv('FEC_CF_PASSWORD'):
-        ctx.run('cf auth "$FEC_CF_USERNAME_{0}" "$FEC_CF_PASSWORD_{0}".format(space.upper())', echo=True)
+    if login == 'True':
+        ctx.run('cf auth "$FEC_CF_USERNAME_{0}" "$FEC_CF_PASSWORD_{0}"'.format(space.upper()), echo=True)
 
     # Target space
     ctx.run('cf target -o fec-beta-fec -s {0}'.format(space), echo=True)
