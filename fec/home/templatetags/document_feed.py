efb57d7ce62d4597c64599e5c279af93bdd914bc
Noah Manger <noah.manger@gsa.gov>
2017-03-14 18:30:17 -0700

    Adding support for including ResourcePages in document feeds

--- fec/home/templatetags/document_feed.py
+++ fec/home/templatetags/document_feed.py
@@ -1,8 +1,28 @@
 from django import template
+from itertools import chain
+from operator import attrgetter
+
 from django.conf import settings
-from home.models import DocumentPage
+from home.models import DocumentPage, ResourcePage
+from wagtail.wagtailcore.models import Page, Orderable
 
 register = template.Library()
+
+def get_documents(page, year='', category=''):
+    documents = DocumentPage.objects.child_of(page).live().order_by('-date')
+
+    if year:
+      documents = documents.filter(date__year=year)
+
+    if category:
+        documents = documents.filter(category=category)
+
+    return documents
+
+def get_resource_pages(page, year='', category=''):
+    resource_pages = ResourcePage.objects.child_of(page).live()
+
+    return resource_pages
 
 @register.inclusion_tag('partials/document-feed.html')
 def document_feed(page, request):
@@ -13,14 +33,14 @@ def document_feed(page, request):
 
     year = request.GET.get('year', '')
     category = request.GET.get('category', '')
-    documents = DocumentPage.objects.child_of(page).live().order_by('-date')
-
-    if year:
-      documents = documents.filter(date__year=year)
-
-    if category:
-        documents = documents.filter(category=category)
+    documents = get_documents(page, year=year, category=category)
+    resource_pages = get_resource_pages(page, year=year, category=category)
+    all_documents = sorted(
+      chain(documents, resource_pages),
+      key=attrgetter('date'),
+      reverse=True
+    )
 
     return {
-      'documents': documents
+      'documents': all_documents
     }
