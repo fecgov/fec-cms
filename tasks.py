b386340d9abad633a53baba0e318f05c428f1e89
LindsayYoung <lindsayn.young@gsa.gov>
2017-03-17 12:57:01 -0400

    Add final routing for deployment

--- tasks.py
+++ tasks.py
@@ -73,9 +73,8 @@ def _detect_space(repo, branch=None, yes=False):
 
 DEPLOY_RULES = (
     ('prod', _detect_prod),
-    # ('stage', lambda _, branch: branch.startswith('release')),
-    # ('dev', lambda _, branch: branch == 'develop'),
-    ('stage', lambda _, branch: branch == 'feature/gov-cloud-support'),
+    ('stage', lambda _, branch: branch.startswith('release')),
+    ('dev', lambda _, branch: branch == 'develop'),
 )
 
 
