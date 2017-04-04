29a94595106ac9a690747c349fc88456cd50dd7e
john carroll <jcarroll@fec.gov>
2017-04-04 13:29:30 -0400

    added vacants to templatag file

--- fec/home/templatetags/commissioners.py
+++ fec/home/templatetags/commissioners.py
@@ -2,19 +2,26 @@ import re
 
 from django import template
 from home.models import CommissionerPage
+from django.db.models import Q
 
 register = template.Library()
 
 @register.inclusion_tag('partials/current-commissioners.html')
 def current_commissioners():
-    chair_commissioner = CommissionerPage.objects.filter(commissioner_title__contains='Chair') \
+    #vacant_chair = ""
+    chair_commissioner = CommissionerPage.objects.filter(commissioner_title__startswith='Chair') \
         .exclude(commissioner_title__contains='Vice').first()
     vice_commissioner = CommissionerPage.objects.filter(commissioner_title__startswith='Vice').first()
-    commissioners = CommissionerPage.objects.filter(commissioner_title__exact='', \
-        term_expiration__isnull=True).order_by('last_name')
+    commissioners = CommissionerPage.objects.filter(term_expiration__isnull=True) \
+        .exclude(Q(commissioner_title__startswith='Chair') \
+        | Q(commissioner_title__startswith='Vice')) \
+        .order_by('last_name')
+
+    vacant_seats = range(0, 4 - commissioners.count())
 
     return {
         'chair_commissioner': chair_commissioner,
         'vice_commissioner': vice_commissioner,
-        'commissioners': commissioners
+        'commissioners': commissioners,
+        'vacant_seats' : vacant_seats,
     }
