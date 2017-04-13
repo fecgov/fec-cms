a85b84fe23827f250a3fc9e02e39e0706ace4429
LindsayYoung <lindsayn.young@gsa.gov>
2017-04-13 09:34:12 -0400

    Make info level the default log

--- fec/fec/settings/base.py
+++ fec/fec/settings/base.py
@@ -264,7 +264,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'level': 'INFO',
         },
     },
 }
