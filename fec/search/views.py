12fbc7c4668933e3f6ff7adb843aa1faa0aa8c4f
Noah Manger <noah.manger@gsa.gov>
2017-04-14 12:47:06 -0700

    Handling search faceting

--- fec/search/views.py
+++ fec/search/views.py
@@ -15,14 +15,14 @@ from wagtail.contrib.wagtailsearchpromotions.models import SearchPromotion
 def search_candidates(query):
     path = os.path.join(settings.FEC_API_VERSION, 'candidates', 'search')
     url = parse.urljoin(settings.FEC_API_URL, path)
-    r = requests.get(url, params={'q': query, 'sort': '-receipts', 'per_page': 5, 'api_key': settings.FEC_API_KEY})
-    return r.json()['results']
+    r = requests.get(url, params={'q': query, 'sort': '-receipts', 'per_page': 3, 'api_key': settings.FEC_API_KEY})
+    return r.json()
 
 def search_committees(query):
     path = os.path.join(settings.FEC_API_VERSION, 'committees')
     url = parse.urljoin(settings.FEC_API_URL, path)
-    r = requests.get(url, params={'q': query, 'per_page': 5, 'api_key': settings.FEC_API_KEY})
-    return r.json()['results']
+    r = requests.get(url, params={'q': query, 'per_page': 3, 'sort': '-receipts', 'api_key': settings.FEC_API_KEY})
+    return r.json()
 
 def prev_offset(limit, next_offset):
     if (next_offset - limit) >= 0:
@@ -34,14 +34,16 @@ def search(request):
     limit = 10
     search_query = request.GET.get('query', None)
     offset = request.GET.get('offset', 0)
-    search_results = []
-    candidate_results = []
-    committee_results = []
-    best_bets = []
-    meta = {}
+    search_type = request.GET.getlist('type', ['site'])
+    results = {}
 
-    # Search
-    if search_query:
+    if 'candidates' in search_type and search_query:
+        results['candidates'] = search_candidates(search_query)
+
+    if 'committees' in search_type and search_query:
+        results['committees'] = search_committees(search_query)
+
+    if 'site' in search_type and search_query:
         params = {
             'affiliate': 'betafec_api',
             'access_key': settings.FEC_DIGITALGOV_KEY,
@@ -50,23 +52,22 @@ def search(request):
             'offset': offset
         }
         r = requests.get('https://search.usa.gov/api/v2/search/i14y', params=params)
-        candidate_results = search_candidates(search_query)
-        committee_results = search_committees(search_query)
-        results = r.json()['web']
-        search_results = results['results']
-        best_bets = r.json()['text_best_bets']
-        meta = {
-            'count': results['total'],
-            'next_offset': results['next_offset'],
-            'prev_offset': prev_offset(limit, int(offset))
+        web_results = r.json()['web']
+        results['site'] = {
+            'results': web_results['results'],
+            'best_bets': r.json()['text_best_bets'],
+            'meta': {
+                'count': web_results['total'],
+                'next_offset': web_results['next_offset'],
+                'prev_offset': prev_offset(limit, int(offset))
+            }
         }
+
+        print(results)
 
     return render(request, 'search/search.html', {
         'search_query': search_query,
-        'search_results': search_results,
-        'candidate_results': candidate_results,
-        'committee_results': committee_results,
-        'best_bets': best_bets,
-        'meta': meta,
+        'results': results,
+        'type': search_type,
         'self': {'title': 'Search results'}
     })
