961064fa42fb09d2245894f97d148a5666c00eea
Jonathan Carmack <jonathancarmack@C02QQ0N5FVH8.local>
2017-04-14 10:50:47 -0400

    Deploy logging feature to dev

--- tasks.py
+++ tasks.py
@@ -74,8 +74,8 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     #('stage', lambda _, branch: branch.startswith('release')),
-    ('stage', lambda _, branch: branch == 'develop'),
-    #('dev', lambda _, branch: branch == 'develop'),
+    #('stage', lambda _, branch: branch == 'develop'),
+    ('dev', lambda _, branch: branch == 'feature/logging-modifications'),
 )
 
 
