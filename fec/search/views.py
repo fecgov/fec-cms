3aeed13084ec0ea7e7ed515697182d1f07dc8245
Noah Manger <noah.manger@gsa.gov>
2017-04-14 16:01:49 -0700

    Parse icon for search results and show total count

--- fec/search/views.py
+++ fec/search/views.py
@@ -18,17 +18,32 @@ def search_candidates(query):
     r = requests.get(url, params={'q': query, 'sort': '-receipts', 'per_page': 3, 'api_key': settings.FEC_API_KEY})
     return r.json()
 
+
 def search_committees(query):
     path = os.path.join(settings.FEC_API_VERSION, 'committees')
     url = parse.urljoin(settings.FEC_API_URL, path)
     r = requests.get(url, params={'q': query, 'per_page': 3, 'sort': '-receipts', 'api_key': settings.FEC_API_KEY})
     return r.json()
 
+
 def prev_offset(limit, next_offset):
     if (next_offset - limit) >= 0:
         return next_offset - limit
     else:
         return 0
+
+
+def parse_icon(path):
+    """
+    Parse which icon to show by checking if the URL is at /data/ 
+    But because some /data/ pages aren't data tables, screen those out with
+    the pages list
+    """
+    pages = ['/legal/advisory-opinions/', '/legal/statutes/', '/advanced/']
+    if '/data/' in path and not any(p for p in pages if p in path):
+        return 'data'
+    else:
+        return 'page'
 
 def search(request):
     limit = 5
@@ -55,13 +70,21 @@ def search(request):
         web_results = r.json()['web']
         results['site'] = {
             'results': web_results['results'],
-            'best_bets': r.json()['text_best_bets'],
+            'best_bets': {
+                'results': r.json()['text_best_bets'],
+                'count': len(r.json()['text_best_bets'])
+            },
             'meta': {
                 'count': web_results['total'],
                 'next_offset': web_results['next_offset'],
                 'prev_offset': prev_offset(limit, int(offset))
             }
         }
+
+        for result in results['site']['results']:
+            result['icon'] = parse_icon(result['url'])
+
+    results['count'] = len(results)
 
     return render(request, 'search/search.html', {
         'search_query': search_query,
