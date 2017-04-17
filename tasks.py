2f4bb9dda5dfe90049bf57e1975c4ebb10369bb9
Jonathan Carmack <jonathan.carmack@gsa.gov>
2017-04-17 14:39:31 -0400

    Deploy tasks back to normal

--- tasks.py
+++ tasks.py
@@ -73,9 +73,8 @@ def _detect_space(repo, branch=None, yes=False):
 
 DEPLOY_RULES = (
     ('prod', _detect_prod),
-    #('stage', lambda _, branch: branch.startswith('release')),
-    #('stage', lambda _, branch: branch == 'develop'),
-    ('dev', lambda _, branch: branch == 'feature/logging-modifications'),
+    ('stage', lambda _, branch: branch.startswith('release')),
+    ('dev', lambda _, branch: branch == 'develop'),
 )
 
 
