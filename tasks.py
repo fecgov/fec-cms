616386d08619e5dc4a23eefcd1ca7f7989dc3f78
LindsayYoung <lindsayn.young@gsa.gov>
2017-05-23 16:25:41 -0400

    test deploy

--- tasks.py
+++ tasks.py
@@ -73,7 +73,8 @@ def _detect_space(repo, branch=None, yes=False):
 
 DEPLOY_RULES = (
     ('prod', _detect_prod),
-    ('stage', lambda _, branch: branch.startswith('release')),
+    ('stage', lambda _, branch: branch == 'hotfix/www-in-manifest'),
+    # ('stage', lambda _, branch: branch.startswith('release')),
     ('dev', lambda _, branch: branch == 'develop'),
 )
 
