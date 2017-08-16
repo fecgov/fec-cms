dcbc6ac85a6eba57947c4b0e786e38f28162a721
Carlo Costino <carlo.costino@gsa.gov>
2017-08-16 00:34:16 -0400

    Modified logging code a bit more to account for missing users
    I noticed that the instance property had both the user and user_id on it, so the user was already being preloaded if that information was available.  In the cases it is not, I wanted to make sure that the log statement was still of use and reflected what was taking place even if we could not provide the user info, which is the user associated with the PageRevision being processed.
    
    In the case of copy or delete actions, all PageRevision objects are cycled through and processed.  This results in multiple log statements being generated, but these statements maintain the user and date/time associated with the PageRevision.  Whenever a series of statements like this is output, the last one represents the change/action that had just taken place.
    
    There is probably a better way to track these items, but that will require a bit more research and this at least fixes the immediate issue of content managers not being able to perform certain actions like deleting pages.

--- fec/home/models.py
+++ fec/home/models.py
@@ -188,15 +188,17 @@ def log_user_save(sender, **kwargs):
 @receiver(pre_delete, sender=PageRevision)
 @receiver(post_save, sender=PageRevision)
 def log_revisions(sender, **kwargs):
-    user_id = kwargs.get('instance').user_id
-    if user_id:
-        try:
-            user = User.objects.get(id=user_id)
-            logger.info("page was modified: {0} by user {1}".format(kwargs.get('instance'), user.get_username()))
-        except User.DoesNotExist:
-            logger.info("User not found")
-    else:
-        logger.info("User not found")
+    username = '(user not found)'
+    user = kwargs.get('instance').user
+
+    if user:
+        username = user.get_username()
+
+    logger.info('Page modified: {0} by user {1}'.format(
+        kwargs.get('instance'),
+        username
+    ))
+
 
 
 def user_groups_changed(sender, **kwargs):
