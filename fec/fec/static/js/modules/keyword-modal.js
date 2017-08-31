5c539de53aafbff065c787356f1dc0f466da9d03
xtine <christine@fieryorange.com>
2017-08-30 19:35:52 -0700

    Parse values and control modal

--- fec/fec/static/js/modules/keyword-modal.js
+++ fec/fec/static/js/modules/keyword-modal.js
@@ -1,46 +1,75 @@
 'use strict';
 
 var $ = require('jquery');
+var URI = require('urijs');
+var A11yDialog = require('a11y-dialog');
 
 function KeywordModal() {
-  this.$elm = $('.js-keyword-modal');
+  this.elm = document.querySelector('.js-keyword-modal');
+  this.$elm = $(this.elm);
   this.$form = this.$elm.find('form');
-  this.$inputs = this.$elm.find('input[type="text"]');
-  this.$combinedValue = this.$elm.find('input[type="hidden"]');
+  this.$fields = this.$elm.find('#keywords-any, #keywords-all, #keywords-exact');
+  this.$excludeField = this.$elm.find('#keywords-none');
   this.$submit = this.$elm.find('button[type="submit"]');
   this.$submit.on('click', this.handleSubmit.bind(this));
+
+  this.dialog = new A11yDialog(this.elm);
+
+  this.$elm.on('dialog:show', function() {
+    $('body').css('overflow', 'hidden');
+  });
+
+  this.$elm.on('dialog:hide', function() {
+    $('body').css('overflow', 'scroll');
+  });
 }
 
 KeywordModal.prototype.handleSubmit = function(e) {
   e.preventDefault();
-  var query = this.combineFields();
-  this.$combinedValue.value = query;
-  // this.$form.submit();
+  var combinedValue = this.combineFields();
+  var query = URI(window.location.search)
+    .removeSearch('search')
+    .addSearch('search', combinedValue);
+  window.location.search = query.toString();
+  this.dialog.hide();
 };
 
 KeywordModal.prototype.combineFields = function() {
   var query = '';
   var self = this;
-  this.$inputs.each(function() {
-    if (this.value) {
-      var joiner = this.dataset.operator === 'exclude' ? '&' : '+';
-      query = query + joiner + '(' + self.parseValue(this) + ')';
+
+  // Add all values from the first three fields, joined by plus-signs
+  this.$fields.each(function() {
+    var $input = $(this);
+    if ($input.val() && query) {
+      query = query + '+' + '(' + self.parseValue($input) + ')';
+    } else if ($input.val()) {
+      query = '(' + self.parseValue($input) + ')';
     }
   });
+
+  // If there's a value in the exclude field, wrap the previous query in ()
+  // and add the value with an ampersand
+  if (this.$excludeField.val() && query) {
+    query = '(' + query + ')&(' + self.parseValue(this.$excludeField) + ')';
+  } else if (this.$excludeField.val()) {
+    query = self.parseValue(this.$excludeField);
+  }
 
   return query;
 };
 
-KeywordModal.prototype.parseValue = function(input) {
-  var words = input.value.split(' ');
-  if (input.dataset.operator === 'and') {
+KeywordModal.prototype.parseValue = function($input) {
+  var words = $input.val().split(' ');
+  var operator = $input.data('operator');
+  if (operator === 'and') {
     return words.join('&');
-  } else if (input.dataset.operator === 'or') {
+  } else if (operator === 'or') {
     return words.join('+');
-  } else if (input.dataset.operator === 'exact') {
-    return '"' + input.value + '"';
-  } else if (input.dataset.operator === 'exclude') {
-    return words.join('-');
+  } else if (operator === 'exact') {
+    return '"' + $input.val() + '"';
+  } else if (operator === 'exclude') {
+    return '-' + words.join('-');
   }
 };
 
