c2d7030921f8ed3111b93f8e89636eef154dd806
xtine <christine@fieryorange.com>
2017-08-30 20:22:10 -0700

    Add keyword modal to legal resources landing

--- fec/fec/static/js/pages/legal.js
+++ fec/fec/static/js/pages/legal.js
@@ -1,8 +1,79 @@
 'use strict';
 
-/* global require */
-var FilterPanel = require('../modules/filters/filter-panel').FilterPanel;
-var KeywordModal = require('../modules/keyword-modal').KeywordModal;
+var $ = require('jquery');
+var A11yDialog = require('a11y-dialog');
 
-new FilterPanel();
-new KeywordModal();
+function KeywordModal() {
+  this.elm = document.querySelector('.js-keyword-modal');
+  this.$elm = $(this.elm);
+  this.$form = this.$elm.find('form');
+  this.$fields = this.$elm.find('#keywords-any, #keywords-all, #keywords-exact');
+  this.$excludeField = this.$elm.find('#keywords-none');
+  this.$submit = this.$elm.find('button[type="submit"]');
+  this.$submit.on('click', this.handleSubmit.bind(this));
+  this.dialog = new A11yDialog(this.elm);
+
+  this.$elm.on('dialog:show', function() {
+    $('body').css('overflow', 'hidden');
+  });
+
+  this.$elm.on('dialog:hide', function() {
+    $('body').css('overflow', 'scroll');
+  });
+
+  /** Unique to CMS **/
+  this.$hiddenField = this.$elm.find('input[type=hidden]');
+}
+
+/** Unique to CMS **/
+KeywordModal.prototype.handleSubmit = function(e) {
+  e.preventDefault();
+  var combinedValue = this.combineFields();
+  this.$hiddenField.val(combinedValue);
+  this.$fields.each(function() { $(this).val(); });
+  this.$excludeField.val();
+  this.$form.submit();
+};
+
+KeywordModal.prototype.combineFields = function() {
+  var query = '';
+  var self = this;
+
+  // Add all values from the first three fields, joined by plus-signs
+  this.$fields.each(function() {
+    var $input = $(this);
+    if ($input.val() && query) {
+      query = query + '+' + '(' + self.parseValue($input) + ')';
+    } else if ($input.val()) {
+      query = '(' + self.parseValue($input) + ')';
+    }
+  });
+
+  // If there's a value in the exclude field, wrap the previous query in ()
+  // and add the value with an ampersand
+  if (this.$excludeField.val() && query) {
+    query = '(' + query + ')&(' + self.parseValue(this.$excludeField) + ')';
+  } else if (this.$excludeField.val()) {
+    query = self.parseValue(this.$excludeField);
+  }
+
+  return query;
+};
+
+KeywordModal.prototype.parseValue = function($input) {
+  var words = $input.val().split(' ');
+  var operator = $input.data('operator');
+  if (operator === 'and') {
+    return words.join('&');
+  } else if (operator === 'or') {
+    return words.join('+');
+  } else if (operator === 'exact') {
+    return '"' + $input.val() + '"';
+  } else if (operator === 'exclude') {
+    return '-' + words.join('-');
+  }
+};
+
+if ($('.js-keyword-modal').length > 0) {
+  new KeywordModal();
+}
