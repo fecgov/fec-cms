7491a893129bde8bc356379a9bb16c2d15780d5b
xtine <christine@fieryorange.com>
2017-08-30 19:39:09 -0700

    Adding documentation

--- fec/fec/static/js/modules/keyword-modal.js
+++ fec/fec/static/js/modules/keyword-modal.js
@@ -4,6 +4,15 @@ var $ = require('jquery');
 var URI = require('urijs');
 var A11yDialog = require('a11y-dialog');
 
+/**
+ * KeywordModal
+ * @class
+ * Creates a pop-up modal for advanced keyword searches
+ * and processes the values of the various inputs to form a single search string
+ * with all the proper boolean operators
+ * In the web app, it loads results by simply adding the new search query
+ * to the window.location.search which forces a page refresh.
+ */
 function KeywordModal() {
   this.elm = document.querySelector('.js-keyword-modal');
   this.$elm = $(this.elm);
@@ -24,6 +33,12 @@ function KeywordModal() {
   });
 }
 
+/**
+ * Handle a click event on the submit button
+ * prevents the form from being submitted at first in order to create the search string
+ * and then replaces the existing search param in the window.
+ * Hides the modal after executed.
+ */
 KeywordModal.prototype.handleSubmit = function(e) {
   e.preventDefault();
   var combinedValue = this.combineFields();
@@ -34,11 +49,17 @@ KeywordModal.prototype.handleSubmit = function(e) {
   this.dialog.hide();
 };
 
+/**
+ * Combine the values of all the inputs into a single formatted string
+ * It first goes through the first three inputs and joins them with "or" operators
+ * If there's an exclude value, it wraps the previous query in parens
+ * and adds the exclude value with an ampersand
+ * @return {string} The combined query
+ */
 KeywordModal.prototype.combineFields = function() {
   var query = '';
   var self = this;
 
-  // Add all values from the first three fields, joined by plus-signs
   this.$fields.each(function() {
     var $input = $(this);
     if ($input.val() && query) {
@@ -48,8 +69,6 @@ KeywordModal.prototype.combineFields = function() {
     }
   });
 
-  // If there's a value in the exclude field, wrap the previous query in ()
-  // and add the value with an ampersand
   if (this.$excludeField.val() && query) {
     query = '(' + query + ')&(' + self.parseValue(this.$excludeField) + ')';
   } else if (this.$excludeField.val()) {
@@ -59,13 +78,18 @@ KeywordModal.prototype.combineFields = function() {
   return query;
 };
 
+/**
+ * Parses the values of the individual input, combining the words with
+ * whichever operator the input requires, as determined by its data-operator attr
+ * @returns {string} The various words joined together with the correct operator
+ */
 KeywordModal.prototype.parseValue = function($input) {
   var words = $input.val().split(' ');
   var operator = $input.data('operator');
   if (operator === 'and') {
     return words.join('&');
   } else if (operator === 'or') {
-    return words.join('+');
+    return words.join('OR');
   } else if (operator === 'exact') {
     return '"' + $input.val() + '"';
   } else if (operator === 'exclude') {
