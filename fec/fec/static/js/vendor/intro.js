d9cfd44a734b9727bb87dfb4895cbb3989d3f57f
xtine <christine@fieryorange.com>
2017-09-08 09:58:02 -0700

    introjs code comment
    instead of just removing tooltip positioning code, just comment it out
    for potential future reference.

--- fec/fec/static/js/vendor/intro.js
+++ fec/fec/static/js/vendor/intro.js
@@ -597,7 +597,100 @@
     windowSize    = _getWinSize();
 
     // Edit by Christine Cheung (xtine)
-    // remove tooltip positioning so it it can be manually set via css
+    // comment out tooltip positioning so it it can be manually set via css
+    //
+    // switch (currentTooltipPosition) {
+    //   case 'top':
+    //     arrowLayer.className = 'introjs-arrow bottom';
+
+    //     if (hintMode) {
+    //       var tooltipLayerStyleLeft = 0;
+    //     } else {
+    //       var tooltipLayerStyleLeft = 15;
+    //     }
+
+    //     _checkRight(targetOffset, tooltipLayerStyleLeft, tooltipOffset, windowSize, tooltipLayer);
+    //     tooltipLayer.style.bottom = (targetOffset.height +  20) + 'px';
+    //     break;
+    //   case 'right':
+    //     tooltipLayer.style.left = (targetOffset.width + 20) + 'px';
+    //     if (targetOffset.top + tooltipOffset.height > windowSize.height) {
+    //       // In this case, right would have fallen below the bottom of the screen.
+    //       // Modify so that the bottom of the tooltip connects with the target
+    //       arrowLayer.className = "introjs-arrow left-bottom";
+    //       tooltipLayer.style.top = "-" + (tooltipOffset.height - targetOffset.height - 20) + "px";
+    //     } else {
+    //       arrowLayer.className = 'introjs-arrow left';
+    //     }
+    //     break;
+    //   case 'left':
+    //     if (!hintMode && this._options.showStepNumbers == true) {
+    //       tooltipLayer.style.top = '15px';
+    //     }
+
+    //     if (targetOffset.top + tooltipOffset.height > windowSize.height) {
+    //       // In this case, left would have fallen below the bottom of the screen.
+    //       // Modify so that the bottom of the tooltip connects with the target
+    //       tooltipLayer.style.top = "-" + (tooltipOffset.height - targetOffset.height - 20) + "px";
+    //       arrowLayer.className = 'introjs-arrow right-bottom';
+    //     } else {
+    //       arrowLayer.className = 'introjs-arrow right';
+    //     }
+    //     tooltipLayer.style.right = (targetOffset.width + 20) + 'px';
+
+    //     break;
+    //   case 'floating':
+    //     arrowLayer.style.display = 'none';
+
+    //     //we have to adjust the top and left of layer manually for intro items without element
+    //     tooltipLayer.style.left   = '50%';
+    //     tooltipLayer.style.top    = '50%';
+    //     tooltipLayer.style.marginLeft = '-' + (tooltipOffset.width / 2)  + 'px';
+    //     tooltipLayer.style.marginTop  = '-' + (tooltipOffset.height / 2) + 'px';
+
+    //     if (typeof(helperNumberLayer) != 'undefined' && helperNumberLayer != null) {
+    //       helperNumberLayer.style.left = '-' + ((tooltipOffset.width / 2) + 18) + 'px';
+    //       helperNumberLayer.style.top  = '-' + ((tooltipOffset.height / 2) + 18) + 'px';
+    //     }
+
+    //     break;
+    //   case 'bottom-right-aligned':
+    //     arrowLayer.className      = 'introjs-arrow top-right';
+
+    //     var tooltipLayerStyleRight = 0;
+    //     _checkLeft(targetOffset, tooltipLayerStyleRight, tooltipOffset, tooltipLayer);
+    //     tooltipLayer.style.top    = (targetOffset.height +  20) + 'px';
+    //     break;
+
+    //   case 'bottom-middle-aligned':
+    //     arrowLayer.className      = 'introjs-arrow top-middle';
+
+    //     var tooltipLayerStyleLeftRight = targetOffset.width / 2 - tooltipOffset.width / 2;
+
+    //     // a fix for middle aligned hints
+    //     if (hintMode) {
+    //       tooltipLayerStyleLeftRight += 5;
+    //     }
+
+    //     if (_checkLeft(targetOffset, tooltipLayerStyleLeftRight, tooltipOffset, tooltipLayer)) {
+    //       tooltipLayer.style.right = null;
+    //       _checkRight(targetOffset, tooltipLayerStyleLeftRight, tooltipOffset, windowSize, tooltipLayer);
+    //     }
+    //     tooltipLayer.style.top = (targetOffset.height + 20) + 'px';
+    //     break;
+
+    //   case 'bottom-left-aligned':
+    //   // Bottom-left-aligned is the same as the default bottom
+    //   case 'bottom':
+    //   // Bottom going to follow the default behavior
+    //   default:
+    //     arrowLayer.className = 'introjs-arrow top';
+
+    //     var tooltipLayerStyleLeft = 0;
+    //     _checkRight(targetOffset, tooltipLayerStyleLeft, tooltipOffset, windowSize, tooltipLayer);
+    //     tooltipLayer.style.top    = (targetOffset.height +  20) + 'px';
+    //     break;
+    // }
   }
 
   /**
