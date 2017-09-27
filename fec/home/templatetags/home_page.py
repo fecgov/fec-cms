6c416b05020644bb2525320caa2d1886cc380abf
john carroll <jcarroll@fec.gov>
2017-09-26 20:19:26 -0400

    templatetag edits to filter active/inactive dates

--- fec/home/templatetags/home_page.py
+++ fec/home/templatetags/home_page.py
@@ -1,6 +1,7 @@
 import re
 
 import datetime
+
 from django import template
 from django.conf import settings
 from operator import attrgetter
@@ -16,10 +17,9 @@ register = template.Library()
 def home_page_banner_announcement():
     on = datetime.datetime.today()
     banners = HomePageBannerAnnouncement.objects.live().filter(active=True,date_active__lte=on, date_inactive__gt=on).order_by('-date_active')[:2]
-    #banners = HomePageBannerAnnouncement.objects.live().filter(active=True,date_active__lte=date.today(), date_inactive__gt=date.today()).order_by('-date_active')[:2]
 
     return {
-        'banners': banners
+        'banners': banners,
     }
 
 
