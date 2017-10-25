8f5f156434d0fd199e68437db298bca5581ec7ed
Pat Phongsvirajati <pphongsvirajati@fec.gov>
2017-10-25 00:13:47 -0400

    set standard offset, account for from_hit boundaries

--- fec/fec/static/js/legal/Pagination.js
+++ fec/fec/static/js/legal/Pagination.js
@@ -1,8 +1,22 @@
 const React = require('react');
 
 function Pagination(props) {
-  function interceptHandleChange(increment) {
-    const syntheticEvent = { target: { name: 'from_hit', value: props.from_hit + increment } };
+  function interceptHandleChange(offset) {
+    
+    // from_hit is a zero-based index
+    var updatedFromHit = props.from_hit + offset;
+
+    // when from_hit is less than 0, reset to 0 (ensures no negative value)
+    if (updatedFromHit < 0){
+      updatedFromHit = 0;
+    } 
+    // ensures that from_hit is less than result count
+    else if (updatedFromHit >= props.resultCount) {
+      updatedFromHit = props.resultCount - 1;
+    }
+
+    // create an event to update from_hit
+    const syntheticEvent = { target: { name: 'from_hit', value: updatedFromHit } };
     props.handleChange(syntheticEvent);
   }
 
@@ -11,10 +25,10 @@ function Pagination(props) {
           {props.from_hit + 1}&ndash;{props.from_hit + props.advisory_opinions.length} of {props.resultCount}</div>
           <div className="dataTables_paginate">
             {props.from_hit > 0 ? <a className="paginate_button previous"
-             onClick={() => interceptHandleChange(-props.advisory_opinions.length)}>Previous</a>
+             onClick={() => interceptHandleChange(-20)}>Previous</a>
               : <span className="paginate_button previous is-disabled">Previous</span> }
             {props.from_hit + props.advisory_opinions.length < props.resultCount ? <a className="paginate_button next"
-             onClick={() => interceptHandleChange(props.advisory_opinions.length)}>Next</a>
+             onClick={() => interceptHandleChange(20)}>Next</a>
               : <span className="paginate_button next is-disabled">Next</span> }
           </div>
         </div> : null
