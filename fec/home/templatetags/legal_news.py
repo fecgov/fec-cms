ee11d1d00202e8d570fb928c97f20105d77ab70f
john carroll <jcarroll@fec.gov>
2017-11-18 19:54:22 -0500

    added feed--list__date class, removed a comment

--- fec/home/templatetags/legal_news.py
+++ fec/home/templatetags/legal_news.py
@@ -8,7 +8,6 @@ register = template.Library()
 
 @register.inclusion_tag('partials/legal-news.html')
 def legal_news_feed():
-    # get one of each update type (not featured)
 
     records = RecordPage.objects.live().filter(homepage_hide=False, homepage_pin=False).order_by('-date')[:4]
 
