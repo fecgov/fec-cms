abd327e16453b710b638bf571e7872c4cee943cc
Carlo Costino <carlo.costino@gsa.gov>
2018-01-12 16:16:54 -0500

    Adding AWS_S3_CUSTOM_DOMAIN back into the settings

--- fec/fec/settings/base.py
+++ fec/fec/settings/base.py
@@ -253,6 +253,7 @@ if FEC_CMS_ENVIRONMENT != 'LOCAL':
     AWS_SECRET_ACCESS_KEY = env.get_credential('secret_access_key')
     AWS_STORAGE_BUCKET_NAME = env.get_credential('bucket')
     AWS_S3_REGION_NAME = env.get_credential('region')
+    AWS_S3_CUSTOM_DOMAIN = env.get_credential('CMS_AWS_CUSTOM_DOMAIN')
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_LOCATION = 'cms-content'
 
