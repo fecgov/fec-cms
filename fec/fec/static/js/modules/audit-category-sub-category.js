c87eb8da2154c5d12d4f3b3108007e323366a692
john carroll <jcarroll@fec.gov>
2018-02-12 01:47:54 -0500

    primary/sub category indication, highlighting, other js changes

--- fec/fec/static/js/modules/audit-category-sub-category.js
+++ fec/fec/static/js/modules/audit-category-sub-category.js
@@ -1,4 +1,6 @@
 'use strict';
+var _ = require('underscore');
+
 /**
  * two dropdowns:#primary_category_id and #sub_category_id
  * when select one option from #primary_category_id dropdown, trigger change function
@@ -8,15 +10,47 @@
 var $ = require('jquery');
 var helpers = require('./helpers');
 
+var $span = $("<span>", {id: "selected-category", class: "t-bold"}).appendTo('.data-container__tags');
+var $subspan = $("<span>", {id: "selected-sub-category", class: "t-bold"}).insertAfter('#selected-category');
+//$(".list--numbered li ol li").css('background-color','none')
+
+var primary_selected
+
 $("#primary_category_id").change(function(event){
   var $select = $('#sub_category_id');
   $.getJSON(helpers.buildUrl(['audit-category'],{'primary_category_id': $('#primary_category_id').val(), 'per_page': 100}),function(data){
     $select.html('');
     $.each(data.results[0].sub_category_list, function(key, val){
-      $select.append('<option selected="selected" value="' + val.sub_category_id + '">' + val.sub_category_name + '</option>');
+      $select.append('<option value="' + val.sub_category_id + '">' + val.sub_category_name + '</option>');
     });
-  });
+    $select.prepend('<option selected value="">Choose a sub-category</option>');
+  })
   if ($("#primary_category_id").val() == -1){
   	$("#sub_category_id").val("-2");
   }
-});
+  primary_selected = $("#primary_category_id option:selected").text()
+  $('#selected-category').html("Primary category: "+ primary_selected + "<br>")
+})
+
+function show_sub_category(){
+    var sub_selected = $("#sub_category_id option:selected").text()
+    //var target = "li ol li:contains('" + sub_selected + "')";
+    var target =
+      $('.list--numbered li ol li').filter(function() {
+      return $(this).text() === sub_selected;
+       });
+
+    var target_parent =
+       $('.list--numbered li').filter(function() {
+       return $($(this).contents()[0]).text() === primary_selected;
+    });
+
+    $(".list--numbered").find(target_parent).find(target).css({'background-color':'#f6f9a4', 'border-radius':'6pc'});
+    $('#selected-sub-category').html("  Subcategory: <span>"+ sub_selected +"</span>")
+    sub_selected == "Choose a sub-category" ? $('#selected-sub-category span').css('color',"red") : $('#selected-sub-category span').css('color',"#000")
+
+}
+
+$(document).bind('ready ajaxComplete', '#sub_category_id', _.debounce(show_sub_category, 250));
+
+
