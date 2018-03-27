beb206c0d3c7712a4bbb120ce8d1772ebf4231ad
john carroll <jcarroll@fec.gov>
2018-03-27 11:33:48 -0400

    review changes first pass

--- fec/home/templatetags/open_jobs.py
+++ fec/home/templatetags/open_jobs.py
@@ -44,6 +44,10 @@ def get_jobs():
     if 'SearchResultItems' in search_results:
         for result in search_results.get('SearchResultItems', None):
             matched_object_descriptor = result.get('MatchedObjectDescriptor', {})
+            if matched_object_descriptor.get('JobGrade')[0]:
+                job_grade = matched_object_descriptor.get('JobGrade', [])[0].get('Code', '')
+            else:
+                job_grade = ''
 
             jobs_dict = {
                 'position_title': matched_object_descriptor.get('PositionTitle', ''),
@@ -51,18 +55,19 @@ def get_jobs():
                 'position_uri': matched_object_descriptor.get('PositionURI', ''),
                 'position_start_date': dateutil.parser.parse(matched_object_descriptor.get('PositionStartDate', '')),
                 'position_end_date': dateutil.parser.parse(matched_object_descriptor.get('PositionEndDate', '')),
-                'job_grade': matched_object_descriptor.get('JobGrade', {})[0].get('Code', ''),
+                'job_grade' : job_grade,#'job_grade': matched_object_descriptor.get('JobGrade', [])[0].get('Code', ''),
                 'low_grade': matched_object_descriptor.get('UserArea', {}).get('Details', {}).get('LowGrade', ''),
                 'high_grade': matched_object_descriptor.get('UserArea', {}).get('Details', {}).get('HighGrade', '')
             }
             #map hiring-path code(s) for each job to description(s)
-            hiring_path_codes = codes_responses['CodeList'][0]['ValidValue']
+            hiring_path_codes = codes_responses.get('CodeList', [])[0].get('ValidValue')#codes_responses['CodeList'][0]['ValidValue']
 
-            hiring_path = [item for item in result['MatchedObjectDescriptor']['UserArea']['Details']['HiringPath']]
+            #hiring_path = [item for item in result['MatchedObjectDescriptor']['UserArea']['Details']['HiringPath']]
+            hiring_path = [item for item in result.get('MatchedObjectDescriptor', {}).get('UserArea', {}).get('Details', {}).get('HiringPath', [])]
             hp = []
             for path in hiring_path:
                 hpa = [item for item in hiring_path_codes if item['Code'] == path.upper()]
-                hp.append(hpa[0]['Value'])
+                hp.append(hpa[0].get('Value', ''))#(hpa[0]['Value'])
                 hiring_path_list = ', '.join(str(n) for n in hp)
                 open_to = {'open_to':hiring_path_list}
             jobs_dict.update(open_to)
