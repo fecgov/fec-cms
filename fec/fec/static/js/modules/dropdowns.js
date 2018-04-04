f8132ddcddd5d53add6f42b06e06d1d5c38add3f
john carroll <jcarroll@fec.gov>
2018-04-04 15:07:00 -0400

    cleanup on reverting dropdown.js

--- fec/fec/static/js/modules/dropdowns.js
+++ fec/fec/static/js/modules/dropdowns.js
@@ -90,7 +90,6 @@ Dropdown.prototype.handleFocusAway = function(e) {
       !this.$panel.is($target) && !$target.is(this.$button)) {
     this.hide();
   }
-
 };
 
 Dropdown.prototype.handleKeyup = function(e) {
