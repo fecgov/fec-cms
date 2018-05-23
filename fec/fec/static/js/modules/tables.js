fc4f9b633b1bcb24dcb93290871cab9da61d01b5
john carroll <jcarroll@fec.gov>
2018-05-23 19:24:51 -0400

    committing the remaining files

--- fec/fec/static/js/modules/tables.js
+++ fec/fec/static/js/modules/tables.js
@@ -32,7 +32,7 @@ var downloadCapFormatted = helpers.formatNumber(DOWNLOAD_CAP);
 var MAX_DOWNLOADS = 5;
 var DOWNLOAD_MESSAGES = {
   recordCap:
-    'Use <a href="' + window.BASE_PATH + '/advanced?tab=bulk-data">' +
+    'Use <a href="' + window.BASE_PATH + '/advanced?tab=other">' +
     'bulk data</a> to export more than ' +
     downloadCapFormatted +
     ' records.',
@@ -91,6 +91,10 @@ function mapSort(order, columns) {
 }
 
 function getCount(response) {
+  // for audit data set, retrun real data result rows
+  if (window.location.pathname === '/legal-resources/enforcement/audit-search/'){
+    return response.pagination.count;
+  }
   var pagination_count = response.pagination.count;
 
   if (response.pagination.count > 1000) {
@@ -471,7 +475,8 @@ DataTable.prototype.initFilters = function() {
       resultType: 'results',
       showResultCount: true
     });
-    this.$widgets.find('.js-filter-tags').prepend(tagList.$body);
+    this.$widgets.find('.js-filter-tags').prepend(tagList.
+      $body);
     this.filterPanel = new FilterPanel();
     this.filterSet = this.filterPanel.filterSet;
     $(window).on('popstate', this.handlePopState.bind(this));
