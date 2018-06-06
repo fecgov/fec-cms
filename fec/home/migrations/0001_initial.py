5a37f3b88715959fc6b95918a01d4c852b097061
Annalee Flower Horne <annalee@users.noreply.github.com>
2018-06-06 15:25:00 -0400

    Adding Wagtail's migrations as dependencies for our 'home' migrations, and updating wagtail's module paths.

--- fec/home/migrations/0001_initial.py
+++ fec/home/migrations/0001_initial.py
@@ -7,7 +7,17 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wagtailcore', '0029_unicode_slugfield_dj19'),
+        ('wagtailcore', '0040_page_draft_title'),
+        ('wagtailadmin', '0001_create_admin_access_permissions'),
+        ('wagtaildocs','0007_merge'),
+        ('wagtailembeds','0003_capitalizeverbose'),
+        ('wagtailforms','0003_capitalizeverbose'),
+        ('wagtailimages','0019_delete_filter'),
+        ('wagtailredirects','0005_capitalizeverbose'),
+        ('wagtailsearch','0003_remove_editors_pick'),
+        ('wagtailsearchpromotions','0002_capitalizeverbose'),
+        ('wagtailusers','0006_userprofile_prefered_language'),
+
     ]
 
     operations = [
