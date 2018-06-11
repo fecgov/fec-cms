95657e05ed63c95575168ee298b5fd70b386887c
Patcharee Phongsvirajati <pphongsvirajati@fec.gov>
2018-06-11 12:39:27 -0400

    removed django test from this branch to run later in separate branch, removed dependencies from initial migration so that migrations can run on deploy without failure, added wagtail app migrations to run first on deploy so that the db changes can be made

--- fec/home/migrations/0001_initial.py
+++ fec/home/migrations/0001_initial.py
@@ -3,22 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
-
 class Migration(migrations.Migration):
-
-    dependencies = [
-        ('wagtailcore', '0040_page_draft_title'),
-        ('wagtailadmin', '0001_create_admin_access_permissions'),
-        ('wagtaildocs','0007_merge'),
-        ('wagtailembeds','0003_capitalizeverbose'),
-        ('wagtailforms','0003_capitalizeverbose'),
-        ('wagtailimages','0019_delete_filter'),
-        ('wagtailredirects','0005_capitalizeverbose'),
-        ('wagtailsearch','0003_remove_editors_pick'),
-        ('wagtailsearchpromotions','0002_capitalizeverbose'),
-        ('wagtailusers','0006_userprofile_prefered_language'),
-
-    ]
 
     operations = [
         migrations.CreateModel(
