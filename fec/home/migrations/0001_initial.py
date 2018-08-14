2a3155e7b12526cb9dc741c66694a752360aa949
Patcharee Phongsvirajati <pphongsvirajati@fec.gov>
2018-08-14 11:21:18 -0400

    re-adds wagtailcore dependency to initial, re-adds python tests to circle config, removed additional wagtail app migrations as those have already been applied on deploy to prod, updated tests to include test coverage output

--- fec/home/migrations/0001_initial.py
+++ fec/home/migrations/0001_initial.py
@@ -5,11 +5,19 @@ from django.db import models, migrations
 
 class Migration(migrations.Migration):
 
+    dependencies = [('wagtailcore', '0040_page_draft_title')]
+
     operations = [
         migrations.CreateModel(
             name='HomePage',
             fields=[
-                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
+                ('page_ptr',
+                 models.OneToOneField(
+                     parent_link=True,
+                     auto_created=True,
+                     primary_key=True,
+                     serialize=False,
+                     to='wagtailcore.Page')),
             ],
             options={
                 'abstract': False,
