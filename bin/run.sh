2db244f5e83b66a5e022259cdecee07dc255ce81
Patcharee Phongsvirajati <pphongsvirajati@fec.gov>
2018-08-23 10:48:32 -0400

    faking home migrations since those don't need to run again after the database refresh. Running the rest of the migrations as it's needed for the upgrade to wagtail 2.2.1

--- bin/run.sh
+++ bin/run.sh
@@ -10,7 +10,18 @@ set -o pipefail
 
 cd fec
 # Run migrations
-./manage.py migrate --fake
+./manage.py makemigrations
+./manage.py migrate wagtailadmin
+./manage.py migrate wagtailcore
+./manage.py migrate wagtaildocs
+./manage.py migrate wagtailembeds
+./manage.py migrate wagtailforms
+./manage.py migrate wagtailimages
+./manage.py migrate wagtailredirects
+./manage.py migrate wagtailsearch
+./manage.py migrate wagtailsearchpromotions
+./manage.py migrate wagtailusers
+./manage.py migrate --fake home
 
 # Run application
 gunicorn -k gevent -w 2 fec.wsgi:application
