b346532545d3010d92985bb8f9d201b49d95e908
Laura Beaufort <31420082+lbeaufort@users.noreply.github.com>
2018-10-19 10:11:31 -0400

    PEP8 format

--- fec/data/views.py
+++ fec/data/views.py
@@ -24,17 +24,14 @@ def groupby(values, keygetter):
         ret.setdefault(key, []).append(value)
     return ret
 
-election_durations = {
-    'P': 4,
-    'S': 6,
-    'H': 2,
-}
+
+election_durations = {'P': 4, 'S': 6, 'H': 2}
 
 report_types = {
     'P': 'presidential',
     'S': 'house-senate',
     'H': 'house-senate',
-    'I': 'ie-only'
+    'I': 'ie-only',
 }
 
 
@@ -47,13 +44,23 @@ def to_date(committee, cycle):
 def landing(request):
     top_candidates_raising = api_caller.load_top_candidates('-receipts', per_page=3)
 
-    return render(request, 'landing.jinja', {
-        'title': 'Campaign finance data',
-        'dates': utils.date_ranges(),
-        'top_candidates_raising': top_candidates_raising['results'] if top_candidates_raising else None,
-        'first_of_year': datetime.date(datetime.date.today().year, 1, 1).strftime('%m/%d/%Y'),
-        'last_of_year': datetime.date(datetime.date.today().year, 12, 31).strftime('%m/%d/%Y'),
-    })
+    return render(
+        request,
+        'landing.jinja',
+        {
+            'title': 'Campaign finance data',
+            'dates': utils.date_ranges(),
+            'top_candidates_raising': top_candidates_raising['results']
+            if top_candidates_raising
+            else None,
+            'first_of_year': datetime.date(datetime.date.today().year, 1, 1).strftime(
+                '%m/%d/%Y'
+            ),
+            'last_of_year': datetime.date(datetime.date.today().year, 12, 31).strftime(
+                '%m/%d/%Y'
+            ),
+        },
+    )
 
 
 def search(request):
@@ -72,17 +79,15 @@ def search(request):
         return redirect(url)
     else:
         results = api_caller.load_search_results(query)
-        return render(request, 'search-results.jinja', {
-            'query': query,
-            'title': 'Search results',
-            'results': results
-        })
+        return render(
+            request,
+            'search-results.jinja',
+            {'query': query, 'title': 'Search results', 'results': results},
+        )
 
 
 def advanced(request):
-    return render(request, 'advanced.jinja', {
-        'title': 'Advanced data'
-    })
+    return render(request, 'advanced.jinja', {'title': 'Advanced data'})
 
 
 def get_candidate(candidate_id, cycle, election_full):
@@ -92,8 +97,11 @@ def get_candidate(candidate_id, cycle, election_full):
     """
 
     candidate, committees, cycle = api_caller.load_with_nested(
-        'candidate', candidate_id, 'committees',
-        cycle=cycle, cycle_key='two_year_period',
+        'candidate',
+        candidate_id,
+        'committees',
+        cycle=cycle,
+        cycle_key='two_year_period',
         election_full=election_full,
     )
     # cycle corresponds to the two-year period for which the committee has financial activity.
@@ -101,10 +109,7 @@ def get_candidate(candidate_id, cycle, election_full):
     if election_full and cycle and cycle not in candidate['election_years']:
 
         next_cycle = next(
-            (
-                year for year in sorted(candidate['election_years'])
-                if year > cycle
-            ),
+            (year for year in sorted(candidate['election_years']) if year > cycle),
             max(candidate['election_years']),
         )
 
@@ -116,19 +121,23 @@ def get_candidate(candidate_id, cycle, election_full):
             election_full = False
         # get the next election cycle data for this candidate
         candidate, committees, cycle = api_caller.load_with_nested(
-            'candidate', candidate_id, 'committees',
-            cycle=next_cycle, cycle_key='two_year_period',
+            'candidate',
+            candidate_id,
+            'committees',
+            cycle=next_cycle,
+            cycle_key='two_year_period',
             election_full=election_full,
         )
 
     # Addresses issue#1644 - make any odd year special election an even year
     #  for displaying elections for pulldown menu in Candidate pages
     #  Using Set to ensure no duplicate years in final list
-    even_election_years = list({ year + (year % 2) for year in candidate.get('election_years', []) })
+    even_election_years = list(
+        {year + (year % 2) for year in candidate.get('election_years', [])}
+    )
 
     election_year = next(
-        (year for year in sorted(even_election_years) if year >= cycle),
-        None,
+        (year for year in sorted(even_election_years) if year >= cycle), None
     )
 
     # If the candidate is not running in a future election,
@@ -147,7 +156,7 @@ def get_candidate(candidate_id, cycle, election_full):
         'name': candidate['name'],
         'cycle': cycle,
         'electionFull': election_full,
-        'candidateID': candidate['candidate_id']
+        'candidateID': candidate['candidate_id'],
     }
 
     # In the case of when a presidential or senate candidate has filed
@@ -163,9 +172,7 @@ def get_candidate(candidate_id, cycle, election_full):
 
     # Annotate committees with most recent available cycle
     aggregate_cycles = (
-        list(range(cycle, cycle - duration, -2))
-        if election_full
-        else [cycle]
+        list(range(cycle, cycle - duration, -2)) if election_full else [cycle]
     )
     for committee in committees:
         committee['related_cycle'] = (
@@ -176,7 +183,9 @@ def get_candidate(candidate_id, cycle, election_full):
 
     # Group the committees by designation
     committee_groups = groupby(committees, lambda each: each['designation'])
-    committees_authorized = committee_groups.get('P', []) + committee_groups.get('A', [])
+    committees_authorized = committee_groups.get('P', []) + committee_groups.get(
+        'A', []
+    )
 
     committee_groups = committee_groups
     committees_authorized = committees_authorized
@@ -185,9 +194,7 @@ def get_candidate(candidate_id, cycle, election_full):
     # Get aggregate totals for the financial summary
     # And pass through the data processing utils
     aggregate = api_caller.load_candidate_totals(
-        candidate['candidate_id'],
-        cycle=max_cycle,
-        election_full=election_full,
+        candidate['candidate_id'], cycle=max_cycle, election_full=election_full
     )
     if aggregate:
         raising_summary = utils.process_raising_data(aggregate)
@@ -203,21 +210,20 @@ def get_candidate(candidate_id, cycle, election_full):
     # Get totals for the last two-year period of a cycle for showing on
     # raising and spending tabs
     two_year_totals = api_caller.load_candidate_totals(
-        candidate['candidate_id'],
-        cycle=max_cycle,
-        election_full=False
+        candidate['candidate_id'], cycle=max_cycle, election_full=False
     )
 
     # Get the statements of candidacy
     statement_of_candidacy = api_caller.load_candidate_statement_of_candidacy(
-        candidate['candidate_id'],
-        cycle=cycle
+        candidate['candidate_id'], cycle=cycle
     )
 
     if statement_of_candidacy:
         for statement in statement_of_candidacy:
             # convert string to python datetime and parse for readable output
-            statement['receipt_date'] = datetime.datetime.strptime(statement['receipt_date'], '%Y-%m-%dT%H:%M:%S')
+            statement['receipt_date'] = datetime.datetime.strptime(
+                statement['receipt_date'], '%Y-%m-%dT%H:%M:%S'
+            )
             statement['receipt_date'] = statement['receipt_date'].strftime('%m/%d/%Y')
 
     # Get all the elections
@@ -257,7 +263,7 @@ def get_candidate(candidate_id, cycle, election_full):
         'statement_of_candidacy': statement_of_candidacy,
         'elections': elections,
         'candidate': candidate,
-        'context_vars': context_vars
+        'context_vars': context_vars,
     }
 
 
@@ -285,7 +291,9 @@ def get_committee(committee_id, cycle):
     """
 
     redirect_to_previous = False if cycle else True
-    committee, all_candidates, cycle = api_caller.load_with_nested('committee', committee_id, 'candidates', cycle)
+    committee, all_candidates, cycle = api_caller.load_with_nested(
+        'committee', committee_id, 'candidates', cycle
+    )
 
     # When there are multiple candidate records of various offices (H, S, P)
     # linked to a single committee ID,
@@ -307,7 +315,9 @@ def get_committee(committee_id, cycle):
     for candidate in candidates:
         election_years = []
         for election_year in candidate['election_years']:
-            start_of_election_period = election_year - election_durations[candidate['office']]
+            start_of_election_period = (
+                election_year - election_durations[candidate['office']]
+            )
             if start_of_election_period < cycle and cycle <= election_year:
                 election_years.append(election_year)
         # For each candidate, set related_cycle to the candidate's time period
@@ -415,11 +425,11 @@ def elections_lookup(request):
     cycle = utils.current_cycle()
     cycles = utils.get_cycles(cycle + 4)
 
-    return render(request, 'election-lookup.jinja', {
-        'parent': 'data',
-        'cycles': cycles,
-        'cycle': cycle,
-    })
+    return render(
+        request,
+        'election-lookup.jinja',
+        {'parent': 'data', 'cycles': cycles, 'cycle': cycle},
+    )
 
 
 def elections(request, office, cycle, state=None, district=None):
@@ -438,36 +448,42 @@ def elections(request, office, cycle, state=None, district=None):
     if (state is not None) and (state and state.upper() not in constants.states):
         raise Http404()
 
-    #map/redirect legacy tab names to correct anchor
-    tab = request.GET.get('tab','').replace('/','')
+    # map/redirect legacy tab names to correct anchor
+    tab = request.GET.get('tab', '').replace('/', '')
     legacy_tabs = {
-         'contributions':'#individual-contributions',
-         'totals' : '#candidate-financial-totals',
-         'spending-by-others':'#independent-expenditures'
-         }
+        'contributions': '#individual-contributions',
+        'totals': '#candidate-financial-totals',
+        'spending-by-others': '#independent-expenditures',
+    }
 
     if tab in legacy_tabs.keys():
         if office == 'senate' or office == 'house':
             return redirect(
-               reverse('elections-state-district', args=(office,state,district,cycle)) + legacy_tabs[tab]
-               )
+                reverse(
+                    'elections-state-district', args=(office, state, district, cycle)
+                )
+                + legacy_tabs[tab]
+            )
         if office == 'president':
             return redirect(
-               reverse('elections-president', args=(office,cycle)) + legacy_tabs[tab]
-               )
+                reverse('elections-president', args=(office, cycle)) + legacy_tabs[tab]
+            )
 
-    return render(request, 'elections.jinja', {
-        'office': office,
-        'office_code': office[0],
-        'parent': 'data',
-        'cycle': cycle,
-        'cycles': cycles,
-        'state': state,
-        'state_full': constants.states[state.upper()] if state else None,
-        'district': district,
-        'title': utils.election_title(cycle, office, state, district),
-
-    })
+    return render(
+        request,
+        'elections.jinja',
+        {
+            'office': office,
+            'office_code': office[0],
+            'parent': 'data',
+            'cycle': cycle,
+            'cycles': cycles,
+            'state': state,
+            'state_full': constants.states[state.upper()] if state else None,
+            'district': district,
+            'title': utils.election_title(cycle, office, state, district),
+        },
+    )
 
 
 def raising(request):
@@ -480,7 +496,9 @@ def raising(request):
     elif top_category in ['party']:
         top_raisers = api_caller.load_top_parties('-receipts', cycle=cycle, per_page=10)
     else:
-        top_raisers = api_caller.load_top_candidates('-receipts', office=top_category, cycle=cycle, per_page=10)
+        top_raisers = api_caller.load_top_candidates(
+            '-receipts', office=top_category, cycle=cycle, per_page=10
+        )
 
     if cycle == datetime.datetime.today().year:
         coverage_end_date = datetime.datetime.today()
@@ -489,17 +507,21 @@ def raising(request):
 
     page_info = top_raisers['pagination']
 
-    return render(request, 'raising-bythenumbers.jinja', {
-        'parent': 'data',
-        'title': 'Raising: by the numbers',
-        'top_category': top_category,
-        'coverage_start_date': datetime.date(cycle - 1, 1, 1),
-        'coverage_end_date': coverage_end_date,
-        'cycles': cycles,
-        'cycle': cycle,
-        'top_raisers': top_raisers['results'],
-        'page_info': utils.page_info(top_raisers['pagination'])
-    })
+    return render(
+        request,
+        'raising-bythenumbers.jinja',
+        {
+            'parent': 'data',
+            'title': 'Raising: by the numbers',
+            'top_category': top_category,
+            'coverage_start_date': datetime.date(cycle - 1, 1, 1),
+            'coverage_end_date': coverage_end_date,
+            'cycles': cycles,
+            'cycle': cycle,
+            'top_raisers': top_raisers['results'],
+            'page_info': utils.page_info(top_raisers['pagination']),
+        },
+    )
 
 
 def spending(request):
@@ -508,28 +530,38 @@ def spending(request):
     cycle = request.GET.get('cycle', constants.DEFAULT_TIME_PERIOD)
 
     if top_category in ['pac']:
-        top_spenders = api_caller.load_top_pacs('-disbursements', cycle=cycle, per_page=10)
+        top_spenders = api_caller.load_top_pacs(
+            '-disbursements', cycle=cycle, per_page=10
+        )
     elif top_category in ['party']:
-        top_spenders = api_caller.load_top_parties('-disbursements', cycle=cycle, per_page=10)
+        top_spenders = api_caller.load_top_parties(
+            '-disbursements', cycle=cycle, per_page=10
+        )
     else:
-        top_spenders = api_caller.load_top_candidates('-disbursements', office=top_category, cycle=cycle, per_page=10)
+        top_spenders = api_caller.load_top_candidates(
+            '-disbursements', office=top_category, cycle=cycle, per_page=10
+        )
 
     if cycle == datetime.datetime.today().year:
         coverage_end_date = datetime.datetime.today()
     else:
         coverage_end_date = datetime.date(cycle, 12, 31)
 
-    return render(request, 'spending-bythenumbers.jinja', {
-        'parent': 'data',
-        'title': 'Spending: by the numbers',
-        'top_category': top_category,
-        'coverage_start_date': datetime.date(cycle - 1, 1, 1),
-        'coverage_end_date': coverage_end_date,
-        'cycles': cycles,
-        'cycle': cycle,
-        'top_spenders': top_spenders['results'],
-        'page_info': utils.page_info(top_spenders['pagination'])
-    })
+    return render(
+        request,
+        'spending-bythenumbers.jinja',
+        {
+            'parent': 'data',
+            'title': 'Spending: by the numbers',
+            'top_category': top_category,
+            'coverage_start_date': datetime.date(cycle - 1, 1, 1),
+            'coverage_end_date': coverage_end_date,
+            'cycles': cycles,
+            'cycle': cycle,
+            'top_spenders': top_spenders['results'],
+            'page_info': utils.page_info(top_spenders['pagination']),
+        },
+    )
 
 
 def feedback(request):
@@ -539,11 +571,24 @@ def feedback(request):
         # '{"param":"value"}'. Needs to be decoded in Python 3
         data = json.loads(request.body.decode("utf-8"))
 
-        if not any([data['action'], data['feedback'], data['about'], data['g-recaptcha-response']]):
+        if not any(
+            [
+                data['action'],
+                data['feedback'],
+                data['about'],
+                data['g-recaptcha-response'],
+            ]
+        ):
             return JsonResponse({'status': False}, status=500)
         else:
             # verify recaptcha
-            verifyRecaptcha = requests.post("https://www.google.com/recaptcha/api/siteverify", data={'secret': settings.FEC_RECAPTCHA_SECRET_KEY, 'response': data['g-recaptcha-response']})
+            verifyRecaptcha = requests.post(
+                "https://www.google.com/recaptcha/api/siteverify",
+                data={
+                    'secret': settings.FEC_RECAPTCHA_SECRET_KEY,
+                    'response': data['g-recaptcha-response'],
+                },
+            )
             recaptchaResponse = verifyRecaptcha.json()
             if not recaptchaResponse['success']:
                 # if captcha failed, return failure
@@ -552,22 +597,26 @@ def feedback(request):
                 # captcha passed, we're ready to submit the issue.
                 title = 'User feedback on ' + request.META.get('HTTP_REFERER')
 
-                body = ("## What were you trying to do and how can we improve it?\n %s \n\n"
-                        "## General feedback?\n %s \n\n"
-                        "## Tell us about yourself\n %s \n\n"
-                        "## Details\n"
-                        "* URL: %s \n"
-                        "* User Agent: %s") % (
-                            data['action'],
-                            data['feedback'],
-                            data['about'],
-                            request.META.get('HTTP_REFERER'),
-                            request.META['HTTP_USER_AGENT'])
+                body = (
+                    "## What were you trying to do and how can we improve it?\n %s \n\n"
+                    "## General feedback?\n %s \n\n"
+                    "## Tell us about yourself\n %s \n\n"
+                    "## Details\n"
+                    "* URL: %s \n"
+                    "* User Agent: %s"
+                ) % (
+                    data['action'],
+                    data['feedback'],
+                    data['about'],
+                    request.META.get('HTTP_REFERER'),
+                    request.META['HTTP_USER_AGENT'],
+                )
 
                 client = github3.login(token=settings.FEC_GITHUB_TOKEN)
-                issue = client.repository('fecgov', 'fec').create_issue(title, body=body)
+                issue = client.repository('fecgov', 'fec').create_issue(
+                    title, body=body
+                )
 
                 return JsonResponse(issue.to_json(), status=201)
     else:
         raise Http404()
-
