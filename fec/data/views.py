1c816d94c825fcde1126a85931059115c1d3a65b
Laura Beaufort <31420082+lbeaufort@users.noreply.github.com>
2018-12-03 09:18:39 -0500

    Add redirect without district for senate election pages

--- fec/data/views.py
+++ fec/data/views.py
@@ -473,14 +473,17 @@ def elections(request, office, cycle, state=None, district=None):
     }
 
     if tab in legacy_tabs.keys():
-        if office == 'senate' or office == 'house':
+        if office == 'house':
             return redirect(
-                reverse(
-                    'elections-state-district', args=(office, state, district, cycle)
-                )
+                reverse('elections-house', args=(office, state, district, cycle))
                 + legacy_tabs[tab]
             )
-        if office == 'president':
+        elif office == 'senate':
+            return redirect(
+                reverse('elections-senate', args=(office, state, cycle))
+                + legacy_tabs[tab]
+            )
+        elif office == 'president':
             return redirect(
                 reverse('elections-president', args=(office, cycle)) + legacy_tabs[tab]
             )
