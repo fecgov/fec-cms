58a1ce9e84fcdfa4fc384ce665629e18e270bc81
john carroll <jcarroll@fec.gov>
2018-12-06 16:24:45 -0500

    removed unused prop on react tooltip, fixed styling spacing on modal window and tooltip

--- fec/fec/static/js/legal/filters/TooltipHelp.js
+++ fec/fec/static/js/legal/filters/TooltipHelp.js
@@ -2,26 +2,17 @@ const React = require('react');
 const PropTypes = require('prop-types')
 
 class TooltipHelp extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { enableTooltip: props.addTooltip}
-  }
   render() {
-
     return (
       <div className="tooltip__container">
        <button className="tooltip__trigger"><span className="u-visually-hidden">Learn more</span></button>
          <div className={`tooltip tooltip--${this.props.verticalPosition} tooltip--${this.props.horizontalPosition}`}>
-            <p className="tooltip--content">{this.props.message}</p>
+            <p className="tooltip--content tooltip__content">{this.props.message}</p>
          </div>
         {this.props.children}
       </div>
     )
   }
 }
-
-
-
-
 
 module.exports = TooltipHelp;
