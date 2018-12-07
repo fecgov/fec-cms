4135a9b35162063fa60e7e98a27fe2607fd288ee
Patcharee Phongsvirajati <pphongsvirajati@fec.gov>
2018-12-07 09:08:55 -0500

    add admin fine canonical page to view

--- fec/legal/views.py
+++ fec/legal/views.py
@@ -78,6 +78,16 @@ def adr_page(request, adr_no):
     })
 
 
+def admin_fine_page(request, admin_fine_no):
+    admin_fine = api_caller.load_legal_admin_fines(admin_fine_no)
+     if not admin_fine:
+        raise Http404()
+     return render(request, 'legal' + '-admin_fine.jinja', {
+        'admin_fine': admin_fine,
+        'parent': 'legal'
+    })
+    
+
 def legal_search(request):
     query = request.GET.get('search', '')
     result_type = request.GET.get('search_type', 'all')
