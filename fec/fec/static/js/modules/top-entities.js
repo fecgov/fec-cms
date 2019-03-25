56eb204cad5b1455da96a824e9784abb07191909
john carroll <jcarroll@fec.gov>
2019-03-25 14:43:37 -0400

    fix linter errors

--- fec/fec/static/js/modules/top-entities.js
+++ fec/fec/static/js/modules/top-entities.js
@@ -179,7 +179,9 @@ TopEntities.prototype.formatData = function(result, rank) {
     rank: rank,
     party: result.party,
     party_code:
-      result.party === null || this.per_page == 3 ? '' : '[' + result.party.toUpperCase() + ']',
+      result.party === null || this.per_page == 3
+        ? ''
+        : '[' + result.party.toUpperCase() + ']',
     url: helpers.buildAppUrl(['candidate', result.candidate_id], {
       cycle: this.election_year,
       election_full: true
