c319148cfa44437760332160e397ca5d8346e55e
Robert Fultz <lashermylasher@yahoo.com>
2019-04-03 11:36:48 -0400

    Polish the animation and design layout

--- fec/fec/static/js/modules/aggregate-totals.js
+++ fec/fec/static/js/modules/aggregate-totals.js
@@ -3,8 +3,16 @@
 const $ = require('jquery');
 const helpers = require('./helpers');
 
+const doInitialNumberBuild = false;
 /**
- * Handles the functionality for the aggregate totals box(es)
+ * Handles the functionality for the aggregate totals box(es).
+ * Loads, creates an <aside> with {@link init}, then makes itself visible (with {@link displayUpdatedData}) after it has some data to show.
+ * @param {String} office - Required. Can be set through data-office for the <script> or collected from the target specified with data-office-control.
+ * @param {String} election_year - Required. Can be set through data-election-year for the <script> or collected from the target specied with data-year-control.
+ * @param {String} officeControl - Required. Set with data-office-control on <script> but not required if data-office is set.
+ * @param {String} yearControl - Required. Set with data-year-control on <script> but not required if data-election-year is set.
+ * @param {String} action - Required. Can be 'raised' or 'spending'? 'spent'?
+ * @param {Boolean} doInitialNumberBuild - Should we animate the first value or just display it and be done? Default: false.
  */
 function AggregateTotals() {
   this.scriptElement; // The <script>
@@ -18,64 +26,84 @@ function AggregateTotals() {
   this.basePath = ['candidates', 'totals', 'by_office'];
   this.baseQuery; // Vars for data load
   this.animVars = {
-    value: 0, // This instance's current value, only used for animation
-    stepCount: 0
+    valueTotal: 0, // This instance's current value, only used for animation
+    valueTemp: 0,
+    stepCount: 0,
+    interval: null
   };
 
   this.init();
 }
 /**
  * Called by {@link loadData} to parse and display the data
- * @param Response queryResponse - The successful API reply
+ * @param {Response} queryResponse - The successful API reply
  */
 AggregateTotals.prototype.displayUpdatedData = function(queryResponse) {
-  console.log('TESTING - query response:');
-  console.log(queryResponse);
+  // Get the office value from the <script>
   this.baseQuery.office = queryResponse.results[0].office;
-  this.baseQuery.election_year = queryResponse.results[0].election_year;
+  // Get the office value from the <script>, but scrub it
+  this.baseQuery.election_year = scrubElectionYear(
+    queryResponse.results[0].election_year,
+    this.baseQuery.office
+  );
 
   // If this is the first build
   if (this.animVars.stepCount == 0) {
-    this.animVars.stepCount = Math.ceil(Math.random() * 10) + 5; // How many animation steps should we have? (random(1-11) + 5)
-    this.animVars.value =
+    this.animVars.stepCount = 1;
+    this.animVars.valueTotal =
       this.action == 'raised'
         ? queryResponse.results[0].total_receipts
         : queryResponse.results[0].total_disbursements;
-    this.animVars.stepAmount = Math.random() * 100 + 123.456; // How much should each step increment? (random(1-11) + 20)
-    this.animVars.startingValue =
-      this.animVars.value - this.animVars.stepAmount * this.animVars.stepCount; // Considering this.animVars.stepAmount, where should we start the animation?
+
+    // Save the first value for subsequent animations
+    this.animVars.valueTemp = this.animVars.valueTotal;
+
+    // If we don't want to animate the first build,
+    if (!doInitialNumberBuild) {
+      this.displayValue(this, this.animVars.valueTotal);
+    }
+
+    // Otherwise, build the initial animation steps
+    else {
+      this.animVars.stepCount = Math.ceil(Math.random() * 10) + 8; // How many animation steps should we have? (random(1-11) + 8)
+
+      this.animVars.stepAmount = Math.random() * 5 + 5; // How much should each step increment? (random(1-6) + 5)
+      this.animVars.startingValue =
+        this.animVars.valueTotal -
+        this.animVars.stepAmount * this.animVars.stepCount; // Considering this.animVars.stepAmount, where should we start the animation?
+
+      // Save the first value for subsequent animations
+      this.animVars.valueTemp = this.animVars.valueTotal;
+
+      // Animating numbers can be messy, and it was here
+      // We're going to set the animation as a series of setTimeout updates
+      // We're going to jump by stepAmount every anim_delay ms for stepCurrent steps
+      // This will handle from $0 where we're jumping from just ~$100 back as well as on control change where we may be jumping up or down by $1B
+      for (
+        this.animVars.stepCurrent = 0;
+        this.animVars.stepCurrent <= this.animVars.stepCount;
+        this.animVars.stepCurrent++
+      ) {
+        let anim_tempVal =
+          this.animVars.startingValue +
+          this.animVars.stepCurrent * this.animVars.stepAmount; // How much this step should display
+        let anim_delay = this.animVars.stepCurrent * 125; // How long this step should wait from the start
+        let instance = this; // The calling instance
+        setTimeout(function() {
+          instance.displayValue(instance, anim_tempVal);
+        }, anim_delay);
+      }
+    }
 
     // Otherwise, only update the steps values
   } else {
-    this.animVars.startingValue = this.animVars.value;
-    this.animVars.value =
+    this.animVars.startingValue = this.animVars.valueTotal;
+    this.animVars.valueTotal =
       this.action == 'raised'
         ? queryResponse.results[0].total_receipts
         : queryResponse.results[0].total_disbursements;
-    this.animVars.stepAmount =
-      (this.animVars.value - this.animVars.startingValue) /
-      this.animVars.stepCount;
-  }
 
-  // Animating numbers can be messy and it was here
-  // We're going to set the animation as a series of setTimeout updates
-  // We're going to jump by stepAmount every anim_delay ms for stepCurrent steps
-  // This will handle from $0 where we're jumping from just ~$100 back as well as on control change where we may be jumping up or down by $1B
-  for (
-    this.animVars.stepCurrent = 0;
-    this.animVars.stepCurrent <= this.animVars.stepCount;
-    this.animVars.stepCurrent++
-  ) {
-    let anim_tempVal =
-      this.animVars.startingValue +
-      this.animVars.stepCurrent * this.animVars.stepAmount; // How much this step should display
-    let anim_delay = this.animVars.stepCurrent * 250; // How long this step should wait from the start
-    let instance = this; // The calling instance
-    setTimeout(function() {
-      let valString =
-        '$' + anim_tempVal.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); // Format for US dollars and cents
-      instance.valueField.innerHTML = valString; // Display this value
-    }, anim_delay);
+    this.startAnimation();
   }
 
   // TODO: let's lean on other sources here
@@ -84,7 +112,7 @@ AggregateTotals.prototype.displayUpdatedData = function(queryResponse) {
     S: 'Senate',
     H: 'House'
   };
-  // Set the description
+  // Set the description text
   this.descriptionField.innerHTML =
     'Total ' +
     this.action +
@@ -136,7 +164,10 @@ AggregateTotals.prototype.init = function() {
       String(this.scriptElement.dataset['yearControl'])
     );
     $(this.yearControl).on('change', this.handleElectionYearChange.bind(this));
-    this.baseQuery.election_year = $(this.yearControl).val();
+    this.baseQuery.election_year = scrubElectionYear(
+      $(this.yearControl).val(),
+      this.baseQuery.office
+    );
 
     // alternatively, we can use a hard-coded single value, too
   } else if (this.scriptElement.dataset['electionYear']) {
@@ -152,7 +183,7 @@ AggregateTotals.prototype.init = function() {
   // If we're missing something, stop here
   if (!allSet && this.action) return;
 
-  // Make the <aside> for the html doc and give it its attributes
+  // Make the <aside> for the dom and give it its attributes
   this.element = document.createElement('aside');
   this.element.setAttribute(
     'id',
@@ -179,27 +210,142 @@ AggregateTotals.prototype.init = function() {
 
 /**
  * Starts the data load, called by {@link init}
- * @param Object query - The data object for the query, {@link baseQuery}
+ * @param {Object} query - The data object for the query, {@link baseQuery}
  */
 AggregateTotals.prototype.loadData = function(query) {
-  console.log('TESTING - loading data query:');
-  console.log(query);
-  let self = this;
+  let instance = this;
   $.getJSON(helpers.buildUrl(this.basePath, query)).done(response => {
-    self.displayUpdatedData(response);
+    instance.displayUpdatedData(response);
   });
 };
 
+/**
+ * Event handler for when the office control is changed.
+ * The target is specified in <script data-office-control />
+ * @param Event e - the change event
+ */
 AggregateTotals.prototype.handleOfficeChange = function(e) {
   e.preventDefault();
   this.baseQuery.office = e.target.value; // Save the updated value
   this.loadData(this.baseQuery); // Reload the data
 };
 
+/**
+ * Event handler for when the election year control changes.
+ * The target is specified in <script data-year-control />
+ * @param {Event} e - The change event
+ */
 AggregateTotals.prototype.handleElectionYearChange = function(e) {
   e.preventDefault();
   this.baseQuery.election_year = e.target.value; // Save the updated value
   this.loadData(this.baseQuery); // Reload the data
 };
+
+/**
+ * Formats the given value and puts it into the dom element.
+ * @param {Element} instance - The dom element whose valueField will hold the text
+ * @param {Number} passedValue - The number to format and plug into the element
+ */
+AggregateTotals.prototype.displayValue = function(instance, passedValue) {
+  let valString =
+    '$' + passedValue.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); // Format for US dollars and cents
+  instance.valueField.innerHTML = valString;
+};
+
+/**
+ * Starts the timers to update the displayed value from one to the next (not part of the initial display)
+ * Called by {@link displayUpdatedData} when the displayed value should changed.
+ */
+AggregateTotals.prototype.startAnimation = function() {
+  let instance = this;
+  // If there's an existing interval, clear it
+  if (instance.animVars.interval) clearInterval(instance.animVars.interval);
+  instance.animVars.interval = setInterval(function() {
+    let nextVal = getNextValue(
+      instance.animVars.valueTemp,
+      instance.animVars.valueTotal
+    );
+    instance.animVars.valueTemp = nextVal; // Save for next loop
+    instance.displayValue(instance, nextVal); // Update the element
+
+    // If our values match, we can stop the animations
+    if (instance.animVars.valueTemp == instance.animVars.valueTotal) {
+      clearInterval(instance.animVars.interval);
+    }
+  }, 25);
+};
+
+/**
+ * Returns the next value to step from `currentValue` to `goalValue`, when animating each place from current to goal.
+ * e.g. changes the ones position from current toward goal, then changes the tens position value from current toward goal, then hundreds, thousands, etc.
+ * @param {*} currentValue
+ * @param {*} goalValue
+ * @returns {Number} - The next value, one step more from currentValue toward goalValue
+ */
+function getNextValue(currentValue, goalValue) {
+  // Convert the values to strings to split them apart into arrays
+  // Multiplying by 100 to get rid of the decimal
+  let currentValArr = Math.round(currentValue * 100)
+    .toString()
+    .split('');
+  let goalValArr = Math.round(goalValue * 100)
+    .toString()
+    .split('');
+
+  // Reversing them will make it easier for us to loop starting with 1-cents, then 10-cents, then 1-dollars, 10-dollars, etc.
+  currentValArr.reverse();
+  goalValArr.reverse();
+
+  // Let's add leading zeroes so the lengths are the same
+  while (goalValArr.length < currentValArr.length) {
+    goalValArr.push('0');
+  }
+  while (currentValArr.length < goalValArr.length) {
+    currentValArr.push('0');
+  }
+
+  for (let i = 0; i < goalValArr.length; i++) {
+    let currentDigitVal = parseInt(currentValArr[i], 10);
+    let goalDigitVal = parseInt(goalValArr[i], 10);
+
+    if (currentDigitVal == goalDigitVal) {
+      // do nothing, just loop
+    } else {
+      // The new digit is one lower than the current one if the goal is lower, but one higher if the goal is higher
+      if (currentDigitVal > goalDigitVal) currentDigitVal--;
+      else if (currentDigitVal < goalDigitVal) currentDigitVal++;
+
+      currentValArr[i] = currentDigitVal.toString();
+
+      // Reverse the array back to normal (no longer need goalValArr)
+      currentValArr.reverse();
+
+      // Make it back into a number
+      // Dividing by 100 to add the decimal back
+      let newTempVal = parseInt(currentValArr.join(''), 10) / 100;
+      newTempVal = Number(newTempVal.toFixed(2));
+      return newTempVal;
+    }
+  }
+  // If there's some kind of error, just return the goal
+  return goalValue;
+}
+
+/**
+ * Returns the next valid election cycle, particularly for presidential races
+ * @param {String, Number} year - The year to scrub / correct
+ * @param {String} office - The office of that cycle
+ * @returns {Number} - Either `office` or the next valid election year
+ */
+function scrubElectionYear(year, office) {
+  let toReturn = year;
+  if (office.toLowerCase() == 'p') {
+    toReturn = parseInt(toReturn);
+    // If election year isn't cleanly divisble by four,
+    // we need to increment it by the difference between its modulus and four
+    if (toReturn % 4) toReturn += 4 - (toReturn % 4);
+  }
+  return toReturn;
+}
 
 new AggregateTotals();
