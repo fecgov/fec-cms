e7ba0ddc42ee33608a67d0872f5f005d2fed5036
Laura Beaufort <31420082+lbeaufort@users.noreply.github.com>
2019-05-31 10:35:00 -0400

    Add 'follow=True' to requests in order to follow redirects

--- fec/data/tests/test_datatables.py
+++ fec/data/tests/test_datatables.py
@@ -10,79 +10,79 @@ class TestDatatablesRender(TestCase):
     # Raising
 
     def test_receipts(self):
-        response = client.get('/data/receipts/')
+        response = client.get('/data/receipts/', follow=True)
         assert response.status_code == 200
 
     def test_individual_contributions(self):
-        response = client.get('/data/individual-contributions/')
+        response = client.get('/data/individual-contributions/', follow=True)
         assert response.status_code == 200
 
     # Spending
 
     def test_disbursements(self):
-        response = client.get('/data/disbursements/')
-        assert response.status_code in (200, 302)
+        response = client.get('/data/disbursements/', follow=True)
+        assert response.status_code == 200
 
     def test_independent_expenditures(self):
-        response = client.get('/data/independent-expenditures/')
+        response = client.get('/data/independent-expenditures/', follow=True)
         assert response.status_code == 200
 
     def test_party_coordinated_expenditures(self):
-        response = client.get('/data/party-coordinated-expenditures/')
+        response = client.get('/data/party-coordinated-expenditures/', follow=True)
         assert response.status_code == 200
 
     def test_electioneering_communications(self):
-        response = client.get('/data/electioneering-communications/')
+        response = client.get('/data/electioneering-communications/', follow=True)
         assert response.status_code == 200
 
     def test_communication_costs(self):
-        response = client.get('/data/communication-costs/')
+        response = client.get('/data/communication-costs/', follow=True)
         assert response.status_code == 200
 
     # Loans and Debts
     def test_loans(self):
-        response = client.get('/data/loans/')
+        response = client.get('/data/loans/', follow=True)
         assert response.status_code == 200
 
     # Candidates
 
     def test_all_candidates(self):
-        response = client.get('/data/candidates/')
+        response = client.get('/data/candidates/', follow=True)
         assert response.status_code == 200
 
     def test_presidential_candidates(self):
-        response = client.get('/data/candidates/president/')
+        response = client.get('/data/candidates/president/', follow=True)
         assert response.status_code == 200
 
     def test_senate_candidates(self):
-        response = client.get('/data/candidates/senate/')
+        response = client.get('/data/candidates/senate/', follow=True)
         assert response.status_code == 200
 
     def test_house_candidates(self):
-        response = client.get('/data/candidates/house/')
+        response = client.get('/data/candidates/house/', follow=True)
         assert response.status_code == 200
 
     # Committees
 
     def test_all_committees(self):
-        response = client.get('/data/committees/')
+        response = client.get('/data/committees/', follow=True)
         assert response.status_code == 200
 
     # Filings and reports
 
     def test_all_filings(self):
-        response = client.get('/data/filings/')
+        response = client.get('/data/filings/', follow=True)
         assert response.status_code == 200
 
     def test_presidential_reports(self):
-        response = client.get('/data/reports/presidential/')
+        response = client.get('/data/reports/presidential/', follow=True)
         assert response.status_code == 200
 
     def test_house_senate_reports(self):
-        response = client.get('/data/reports/house-senate/')
+        response = client.get('/data/reports/house-senate/', follow=True)
         assert response.status_code == 200
 
     def test_pac_party_reports(self):
-        response = client.get('/data/reports/pac-party/')
+        response = client.get('/data/reports/pac-party/', follow=True)
         assert response.status_code == 200
 
