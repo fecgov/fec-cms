c118c23c9b5076c5c300d4e04a972331e694e9d1
Robert Fultz <lashermylasher@yahoo.com>
2019-07-03 09:53:55 -0400

    Deploy to Feature

--- tasks.py
+++ tasks.py
@@ -74,10 +74,10 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     ('stage', lambda _, branch: branch.startswith('release')),
-    # ('dev', lambda _, branch: branch == 'develop'),
-    ('dev', lambda _, branch: branch == 'test/polyfills-for-ie-and-agg-tots')
+    ('dev', lambda _, branch: branch == 'develop'),
+    ('feature', lambda _, branch: branch == 'test/polyfills-for-ie-and-agg-tots')
     # Uncomment below and adjust branch name to deploy desired feature branch to the feature space
-    #('feature', lambda _, branch: branch == 'feature/INSERT_BRANCH_NAME'),
+    # ('feature', lambda _, branch: branch == 'feature/INSERT_BRANCH_NAME'),
 )
 
 
