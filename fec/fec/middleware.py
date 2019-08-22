1cecba3192f928c0f638e0ca4f61b0195b420283
Laura Beaufort <31420082+lbeaufort@users.noreply.github.com>
2019-08-22 17:42:09 -0400

    Minor tweaks to CSP, spacing

--- fec/fec/middleware.py
+++ fec/fec/middleware.py
@@ -3,23 +3,26 @@ from django.conf import settings
 
 
 class SecurityHeaders(MiddlewareMixin):
+    """Add secure headers to each response"""
 
     def process_response(self, request, response):
 
         if settings.FEC_CMS_ENVIRONMENT == settings.ENVIRONMENTS.get('local'):
-            content_security_policy = ''\
-                'default-src \'self\' data: *.fec.gov localhost:* http://127.0.0.1:* http://*.app.cloud.gov https://*.app.cloud.gov; '\
-                'frame-src \'self\' https://www.google.com; '\
-                'img-src \'self\' data: http://*.fastly.net;'\
-                'script-src \'self\' \'unsafe-inline\' \'unsafe-eval\' https://www.google.com https://www.gstatic.com https://polyfill.io; '\
-                'style-src \'self\' data: \'unsafe-inline\'; '
+            content_security_policy = \
+                "default-src 'self' " \
+                "data: *.fec.gov localhost:* http://127.0.0.1:* *.app.cloud.gov; " \
+                "frame-src 'self' https://www.google.com; " \
+                "img-src 'self' data: http://*.fastly.net; " \
+                "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com https://polyfill.io; " \
+                "style-src 'self' data: 'unsafe-inline'; "
         else:
-            content_security_policy = ''\
-                'default-src \'self\' data: *.fec.gov; '\
-                'frame-src \'self\' https://www.google.com;'\
-                'img-src \'self\' data: http://*.fastly.net; '\
-                'script-src \'self\' \'unsafe-inline\' \'unsafe-eval\' https://www.google.com https://www.gstatic.com https://polyfill.io https://dap.digitalgov.gov; '\
-                'style-src \'self\' data: \'unsafe-inline\'; '
-        response['Content-Security-Policy'] = content_security_policy
-        response['cache-control'] = "max-age=600"
+            content_security_policy = \
+                "default-src 'self' " \
+                "data: *.fec.gov *.app.cloud.gov https://www.google-analytics.com; " \
+                "frame-src 'self' https://www.google.com; " \
+                "img-src 'self' data: http://*.fastly.net https://www.google-analytics.com; " \
+                "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.google-analytics.com https://www.gstatic.com https://polyfill.io https://dap.digitalgov.gov; " \
+                "style-src 'self' data: 'unsafe-inline'; "
+        response["Content-Security-Policy"] = content_security_policy
+        response["cache-control"] = "max-age=600"
         return response
