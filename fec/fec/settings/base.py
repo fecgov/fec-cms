1820c5cd6ad31fdab72ebbbc1a138518b8c3ab63
rfultz <lashermylasher@yahoo.com>
2019-08-27 12:20:19 -0400

    Create Tag Manager feature flag

--- fec/fec/settings/base.py
+++ fec/fec/settings/base.py
@@ -50,6 +50,7 @@ FEATURES = {
     'aggregatetotals': bool(env.get_credential('FEC_FEATURE_AGGR_TOTS', '')),
     'map': bool(env.get_credential('FEC_FEATURE_HOME_MAP', '')),
     'barcharts': bool(env.get_credential('FEC_FEATURE_HOME_BARCHARTS', '')),
+    'tagmanager': bool(env.get_credential('FEC_FEATURE_USE_TAG_MANAGER', '')),
 }
 
 ENVIRONMENTS = {
