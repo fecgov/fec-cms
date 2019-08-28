d4710736111e6ddb9f15781a0d7c72f4ce5f95d2
Laura Beaufort <31420082+lbeaufort@users.noreply.github.com>
2019-08-27 20:42:44 -0400

    Add 'object-src', remove 'data:'

--- fec/fec/middleware.py
+++ fec/fec/middleware.py
@@ -8,11 +8,12 @@ class AddSecureHeaders(MiddlewareMixin):
     def process_response(self, request, response):
 
         content_security_policy = {
-            "default-src": "'self' data: *.fec.gov *.app.cloud.gov https://www.google-analytics.com",
+            "default-src": "'self' *.fec.gov *.app.cloud.gov https://www.google-analytics.com",
             "frame-src": "'self' https://www.google.com",
             "img-src": "'self' data: http://*.fastly.net https://www.google-analytics.com",
             "script-src": "'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.google-analytics.com https://www.gstatic.com https://polyfill.io https://dap.digitalgov.gov",
-            "style-src": "'self' data: 'unsafe-inline'"
+            "style-src": "'self' data: 'unsafe-inline'",
+            "object-src": "'none'",
         }
         if settings.FEC_CMS_ENVIRONMENT == settings.ENVIRONMENTS.get('local'):
             content_security_policy["default-src"] += " localhost:* http://127.0.0.1:*"
