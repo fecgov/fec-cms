1c19c480141dcfe41855dc783332184baace6385
Pat Phongsvirajati <pphongsvirajati@fec.gov>
2019-10-16 15:48:14 -0400

    add fec.gov domain to image sources in csp

--- fec/fec/middleware.py
+++ fec/fec/middleware.py
@@ -14,7 +14,7 @@ class AddSecureHeaders(MiddlewareMixin):
         content_security_policy = {
             "default-src": "'self' *.fec.gov *.app.cloud.gov https://www.google-analytics.com",
             "frame-src": "'self' https://www.google.com/recaptcha/ https://www.youtube.com/",
-            "img-src": "'self' data: https://*.ssl.fastly.net https://www.google-analytics.com https://www.googletagmanager.com *.app.cloud.gov",
+            "img-src": "'self' *.fec.gov *.app.cloud.gov data: https://*.ssl.fastly.net https://www.google-analytics.com https://www.googletagmanager.com",
             "script-src": "'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://www.google-analytics.com https://www.googletagmanager.com https://polyfill.io https://dap.digitalgov.gov",
             "style-src": "'self' data: 'unsafe-inline'",
             "object-src": "'none'",
