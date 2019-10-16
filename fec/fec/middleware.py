372b9bf29c832140d1eb8bf493e7d7a7ea53739c
Pat Phongsvirajati <pphongsvirajati@fec.gov>
2019-10-16 16:01:07 -0400

    add wagtail gravatar icon to allowed images

--- fec/fec/middleware.py
+++ fec/fec/middleware.py
@@ -14,7 +14,7 @@ class AddSecureHeaders(MiddlewareMixin):
         content_security_policy = {
             "default-src": "'self' *.fec.gov *.app.cloud.gov https://www.google-analytics.com",
             "frame-src": "'self' https://www.google.com/recaptcha/ https://www.youtube.com/",
-            "img-src": "'self' *.fec.gov *.app.cloud.gov data: https://*.ssl.fastly.net https://www.google-analytics.com https://www.googletagmanager.com",
+            "img-src": "'self' *.fec.gov *.app.cloud.gov data: https://*.ssl.fastly.net https://www.google-analytics.com https://www.googletagmanager.com https://www.gravatar.com/avatar/dd9a1a05d5c70a0ce8317f4172745459?s=100&d=mm",
             "script-src": "'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://www.google-analytics.com https://www.googletagmanager.com https://polyfill.io https://dap.digitalgov.gov",
             "style-src": "'self' data: 'unsafe-inline'",
             "object-src": "'none'",
