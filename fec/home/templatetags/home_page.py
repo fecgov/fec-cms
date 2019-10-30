f3e07b9f70971ab3974dad6d108116c2f0b77ff3
john carroll <jcarroll@fec.gov>
2019-10-30 18:42:39 -0400

    added argument to templatetag toto control layout, removed redundant templatetag in home_page.py

--- fec/home/templatetags/home_page.py
+++ fec/home/templatetags/home_page.py
@@ -70,33 +70,3 @@ def home_page_news():
     return {
         'updates': updates
     }
-
-@register.inclusion_tag('partials/commissioners-home.html')
-def current_commissioners():
-    current_commissioners = CommissionerPage.objects.filter(term_expiration__isnull=True)
-    chair_commissioner = current_commissioners.filter(commissioner_title__startswith='Chair') \
-        .exclude(commissioner_title__contains='Vice').first()
-    vice_commissioner = current_commissioners.filter(commissioner_title__startswith='Vice').first()
-    other_commissioners = current_commissioners \
-        .exclude(Q(commissioner_title__startswith='Chair') \
-        | Q(commissioner_title__startswith='Vice')) \
-        .order_by('last_name')
-
-    # Checks if there are any current commissioners
-    if current_commissioners:
-        try:
-            current_commissioners_count = len(current_commissioners)
-        except:
-            current_commissioners_count = 1
-    else:
-        current_commissioners_count = 0
-
-    vacant_seats = range(0, 6 - current_commissioners_count)
-
-    return {
-        'chair_commissioner': chair_commissioner,
-        'vice_commissioner': vice_commissioner,
-        'commissioners': other_commissioners,
-        'vacant_seats' : vacant_seats,
-    }
-
