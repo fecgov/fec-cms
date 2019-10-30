96cb9e40a4521a3c52559f3849db74a8507d9839
john carroll <jcarroll@fec.gov>
2019-10-30 01:55:54 -0400

    added current_commisioners templatetag to home-page.py for home current commissioners

--- fec/home/templatetags/home_page.py
+++ fec/home/templatetags/home_page.py
@@ -5,12 +5,13 @@ import re
 
 from django import template
 from django.conf import settings
+from django.db.models import Q
 from operator import attrgetter
 from itertools import chain, islice
 from datetime import date
 from pytz import timezone
 from home.models import (HomePageBannerAnnouncement, AlertForEmergencyUseOnly, DigestPage, RecordPage, PressReleasePage,
-                        TipsForTreasurersPage, ServicesLandingPage)
+                        TipsForTreasurersPage, ServicesLandingPage, CommissionerPage)
 
 register = template.Library()
 
@@ -69,3 +70,33 @@ def home_page_news():
     return {
         'updates': updates
     }
+
+@register.inclusion_tag('partials/commissioners-home.html')
+def current_commissioners():
+    current_commissioners = CommissionerPage.objects.filter(term_expiration__isnull=True)
+    chair_commissioner = current_commissioners.filter(commissioner_title__startswith='Chair') \
+        .exclude(commissioner_title__contains='Vice').first()
+    vice_commissioner = current_commissioners.filter(commissioner_title__startswith='Vice').first()
+    other_commissioners = current_commissioners \
+        .exclude(Q(commissioner_title__startswith='Chair') \
+        | Q(commissioner_title__startswith='Vice')) \
+        .order_by('last_name')
+
+    # Checks if there are any current commissioners
+    if current_commissioners:
+        try:
+            current_commissioners_count = len(current_commissioners)
+        except:
+            current_commissioners_count = 1
+    else:
+        current_commissioners_count = 0
+
+    vacant_seats = range(0, 6 - current_commissioners_count)
+
+    return {
+        'chair_commissioner': chair_commissioner,
+        'vice_commissioner': vice_commissioner,
+        'commissioners': other_commissioners,
+        'vacant_seats' : vacant_seats,
+    }
+
