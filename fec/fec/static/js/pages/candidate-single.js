25481b59fd9b91a101262fe45154fcc29ea8b36a
rfultz <lashermylasher@yahoo.com>
2019-11-18 14:58:48 -0500

    Lint

--- fec/fec/static/js/pages/candidate-single.js
+++ fec/fec/static/js/pages/candidate-single.js
@@ -390,7 +390,7 @@ function initSpendingTables() {
         columns: opts.columns,
         order: [[0, 'desc']],
         dom: tables.simpleDOM,
-        pagingType: 'simple', 
+        pagingType: 'simple',
         lengthChange: true,
         pageLength: 10,
         lengthMenu: [10, 50, 100],
