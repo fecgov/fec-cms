e92d48ed1e85ca4d842a7b43ed5d07978ed89155
Pat Phongsvirajati <pphongsvirajati@fec.gov>
2020-03-06 13:35:04 -0500

    test deploy circle pipelines to dev

--- tasks.py
+++ tasks.py
@@ -73,7 +73,8 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     ('stage', lambda _, branch: branch.startswith('release')),
-    ('dev', lambda _, branch: branch == 'develop'),
+    #('dev', lambda _, branch: branch == 'develop'),
+    ('dev', lambda _, branch: branch == 'test/3578-enable-circleci-pipeline'),
     # Uncomment below and adjust branch name to deploy desired feature branch to the feature space
     #('feature', lambda _, branch: branch == 'feature/add-legal-citations-to-full-width-template'),
 )
