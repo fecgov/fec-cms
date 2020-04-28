ffadf634b0f4ac0d0baabedcca278acb61cfe139
Pat Phongsvirajati <pphongsvirajati@fec.gov>
2020-04-28 17:05:37 -0400

    set python get request timeout to 90 seconds

--- fec/data/api_caller.py
+++ fec/data/api_caller.py
@@ -29,7 +29,8 @@ def _call_api(*path_parts, **filters):
 
     path = os.path.join(settings.FEC_API_VERSION, *[x.strip("/") for x in path_parts])
     url = parse.urljoin(settings.FEC_API_URL, path)
-    results = session.get(url, params=filters)
+    timeout = 90
+    results = session.get(url, params=filters, timeout=timeout)
 
     # Log the caller function and API endpoint
     current_frame = inspect.currentframe()
