370f2265aa066296b140abe89e7f500345614ae5
rfultz <lashermylasher@yahoo.com>
2020-06-15 10:13:49 -0400

    Remove commented code from conftest

--- conftest.py
+++ conftest.py
@@ -1,46 +1,6 @@
 import logging
 
 
-# @pytest.fixture(scope="session")
-# def migrate_db(request):
-#     with rest.app.app_context():
-#         rest.app.config['TESTING'] = True
-#         rest.app.config['SQLALCHEMY_DATABASE_URI'] = common.TEST_CONN
-#         reset_schema()
-#         run_migrations()
-#         manage.refresh_materialized(concurrent=False)
-
-
-# def run_migrations():
-#     subprocess.check_call(
-#         [
-#             'flyway',
-#             'migrate',
-#             '-n',
-#             '-url={0}'.format(common.get_test_jdbc_url()),
-#             '-locations=filesystem:data/migrations',
-#         ],
-#     )
-
-
-# def reset_schema():
-#     for schema in [
-#         "aouser",
-#         "auditsearch",
-#         "disclosure",
-#         "fecapp",
-#         "fecmur",
-#         "public",
-#         "rad_pri_user",
-#         "real_efile",
-#         "real_pfile",
-#         "rohan",
-#         "staging",
-#     ]:
-#         rest.db.engine.execute('drop schema if exists %s cascade;' % schema)
-#     rest.db.engine.execute('create schema public;')
-
-
 def pytest_addoption(parser):
     parser.addoption(
         "--no-linting",
