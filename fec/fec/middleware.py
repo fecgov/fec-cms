c0db956cfb5993b6a6039af4ddd4aae36d8a5461
rfultz <lashermylasher@yahoo.com>
2020-10-01 16:04:28 -0400

    Add Wagtail reqs to csp but only for admins

--- fec/fec/middleware.py
+++ fec/fec/middleware.py
@@ -1,5 +1,6 @@
 from django.utils.deprecation import MiddlewareMixin
 from django.conf import settings
+import re
 
 
 class AddSecureHeaders(MiddlewareMixin):
@@ -37,13 +38,13 @@ class AddSecureHeaders(MiddlewareMixin):
                 'self' \
                 'unsafe-inline' \
                 'unsafe-eval' \
+                https://dap.digitalgov.gov \
+                https://polyfill.io/ \
                 https://www.google.com/recaptcha/ \
                 https://ssl.google-analytics.com \
                 https://www.google-analytics.com \
                 https://www.googletagmanager.com \
                 https://www.gstatic.com/recaptcha/ \
-                https://polyfill.io \
-                https://dap.digitalgov.gov \
             ",  # do we need unsafe-eval? (Doesn't it only allow 'eval()'?)
             "style-src": "\
                 'self' \
@@ -54,8 +55,6 @@ class AddSecureHeaders(MiddlewareMixin):
                 'none' \
             "
             # Google's requirements found at https://developers.google.com/tag-manager/web/csp
-            # TODO: Do we have a way to handle reporting CSP violations,
-            # TODO: like this https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP#Enabling_reporting
             #
             # TODO: To get away from unsafe-inline, we could look into hashing our inline script elements:
             # TODO: like this https://content-security-policy.com/hash/
@@ -64,10 +63,15 @@ class AddSecureHeaders(MiddlewareMixin):
             content_security_policy["default-src"] += " localhost:* http://127.0.0.1:*"  # TODO: add filesystem?
 
         if settings.FEC_CMS_ENVIRONMENT != 'PRODUCTION':  # pre-prod environments
-            content_security_policy["script-src"] += " https://tagmanager.google.com"
-            content_security_policy["style-src"] += " https://tagmanager.google.com https://fonts.googleapis.com"
-            content_security_policy["img-src"] += " https://ssl.gstatic.com https://www.gstatic.com"
-            content_security_policy["font-src"] += " https://fonts.gstatic.com data:"
+            content_security_policy["font-src"] += " https://fonts.gstatic.com/ data:"
+            content_security_policy["img-src"] += " https://ssl.gstatic.com/ https://www.gstatic.com/"
+            content_security_policy["script-src"] += " https://tagmanager.google.com/"
+            content_security_policy["style-src"] += " https://tagmanager.google.com/ https://fonts.googleapis.com/"
+
+        # Add specific rules/permissions for users who are logged in (and not for the general site visitor)
+        if request.user.is_authenticated:
+            content_security_policy["img-src"] += " https://www.gravatar.com/avatar/"
+            content_security_policy["connect-src"] += " https://releases.wagtail.io/latest.txt"
 
         # Skip CSP reporting in production so we don't clutter up the logs
         if settings.FEC_CMS_ENVIRONMENT != 'PRODUCTION':
@@ -78,7 +82,7 @@ class AddSecureHeaders(MiddlewareMixin):
             content_security_policy["report-uri"] = report_uri
 
         response["Content-Security-Policy"] = "".join(
-            "{0} {1}; ".format(directive, value)
+            "{0} {1}; ".format(directive, re.sub(' +', ' ', value))
             for directive, value in content_security_policy.items()
         )
         response["cache-control"] = "max-age=600"
