b1c055daadbc0807dffd5ee9fb80f65bfaf8cb1e
John Carroll <jcarroll@macadmins-MBP-4.fios-router.home>
2020-10-08 00:27:35 -0400

    remove out-of-bounds tr index from hide-mapping, was breaking app

--- fec/fec/static/js/pages/statistical-summary.js
+++ fec/fec/static/js/pages/statistical-summary.js
@@ -200,7 +200,7 @@ StatisticalSummary.prototype.showTable = function() {
         case this.chosenYear >= 2012:
           liveTable = document.getElementById('type_1');
           break;
-        case this.chosenYear <= 2010:
+        case this.chosenYear <= 2019:
           liveTable = document.getElementById('type_2');
           break;
       }
@@ -282,7 +282,7 @@ StatisticalSummary.prototype.showTable = function() {
         case this.chosenYear <= 2002 && this.chosenYear >= 1990:
           liveTable = document.getElementById('type_4');
           //Hide rows at these indexes for type_4 for this time period
-          [4, 5].map(x => (liveTable.rows[x].style.display = 'none'));
+          [4].map(x => (liveTable.rows[x].style.display = 'none'));
           break;
       }
       break; //LAST BREAK FOR CATEGORY SWITCH
