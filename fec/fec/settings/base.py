5efaa859dea68f7bf3b04ba2347de081df45ca4a
rfultz <lashermylasher@yahoo.com>
2021-03-16 12:10:31 -0400

    Upgrade Wagtail to 2.11.6

--- fec/fec/settings/base.py
+++ fec/fec/settings/base.py
@@ -130,7 +130,6 @@ MIDDLEWARE = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'wagtail.core.middleware.SiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 )
 
