46baeadcbc6e9c31f3be2295936ecc40f29ecc47
rfultz <lashermylasher@yahoo.com>
2021-05-20 16:07:17 -0400

    Move the listeners into home.js

--- fec/fec/static/js/pages/home.js
+++ fec/fec/static/js/pages/home.js
@@ -20,29 +20,32 @@ function scrubURI() {
     );
   }
 }
-// Let's remove any query params after the page and all resources are loaded
+// After all the files are loaded and ready,
 window.addEventListener('load', () => {
+  // Let's remove any query params after the page
   scrubURI();
-});
 
-// For any elements who want to add query parameters to the URL, remove 'em
-let inputs = document.querySelectorAll('#main input, #main select');
-inputs.forEach(element => {
-  element.addEventListener('change', scrubURI);
-});
+  // and make sure no interactive elements add a query param
+  let inputs = document.querySelectorAll('#main input, #main select');
+  inputs.forEach(element => {
+    element.addEventListener('change', scrubURI);
+  });
 
-//
-let offices = document.querySelectorAll('.js-office');
-offices.forEach(element => {
-  element.value = 'S';
-});
+  // Set the default office value
+  let offices = document.querySelectorAll('.js-office');
+  offices.forEach(element => {
+    element.value = 'S';
+  });
 
-let years = document.querySelectorAll('.js-election-year');
-years.forEach(element => {
-  element.value = '2022';
-});
+  // Set the default election cycle
+  let years = document.querySelectorAll('.js-election-year');
+  years.forEach(element => {
+    element.value = window.DEFAULT_ELECTION_YEAR;
+  });
 
-let toggles = document.querySelectorAll('.js-chart-toggle[value=receipts]');
-toggles.forEach(element => {
-  element.checked = true;
+  // Set the chart toggle to receipts
+  let toggles = document.querySelectorAll('.js-chart-toggle[value=receipts]');
+  toggles.forEach(element => {
+    element.checked = true;
+  });
 });
