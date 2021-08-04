8c7090b78c3ec92633b01f3bc65ce66b4c29a3d2
rfultz <lashermylasher@yahoo.com>
2021-08-04 14:41:48 -0400

    Added new glossary definitions

--- fec/data/views.py
+++ fec/data/views.py
@@ -477,8 +477,8 @@ def get_committee(committee_id, cycle):
         com_type_text = 'trade association'
         com_type_glossary = 'Trade association'
     elif com_org_type == 'V':
-        com_type_text = 'cooperatives'
-        com_type_glossary = ''
+        com_type_text = 'cooperative'
+        com_type_glossary = 'Cooperatives'
     # The three biggies which are also 'not' rules for committee_type
     elif com_com_type == 'H':
         com_type_text = '[H committee_type]'
@@ -523,8 +523,8 @@ def get_committee(committee_id, cycle):
         com_type_text = 'political action committees'
         com_type_glossary = 'Political Action Committee (PAC)'
     elif com_com_type == 'U':
-        com_type_text = '[U committee_type]'
-        com_type_glossary = ''
+        com_type_text = 'single candidate independent expenditure committees'
+        com_type_glossary = 'Single candidate independent expenditure filer'
     elif com_com_type == 'I':
         com_type_text = '[I committee_type]'
         com_type_glossary = ''
