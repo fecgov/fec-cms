7b981035a7adae3d3737617fdd8c0b55a9544418
John Carroll <jcarroll@macadmins-MBP-4.fios-router.home>
2021-10-22 18:22:48 -0400

    revert tasks.py

--- tasks.py
+++ tasks.py
@@ -76,7 +76,7 @@ DEPLOY_RULES = (
     ('stage', lambda _, branch: branch.startswith('release')),
     ('dev', lambda _, branch: branch == 'develop'),
     # Uncomment below and adjust branch name to deploy desired feature branch to the feature space
-    ('feature', lambda _, branch: branch == 'feature/4912-admin-fines-report_type_full'),
+    # ('feature', lambda _, branch: branch == '[BRANCH NAME]'),
 )
 
 
