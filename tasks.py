f4d0f0708ea926739384e1d9a606827a81a3dbf7
John Carroll <jcarroll@macadmins-MacBook-Pro-4.local>
2022-04-08 11:50:06 -0400

    rm task for dev deploy

--- tasks.py
+++ tasks.py
@@ -74,7 +74,7 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     ('stage', lambda _, branch: branch.startswith('release')),
-    ('dev', lambda _, branch: branch == 'feature/5124-feedback-box-errors-FullWidth-template'),
+    ('dev', lambda _, branch: branch == 'develop'),
     # Uncomment below and adjust branch name to deploy desired feature branch to the feature space
     # ('feature', lambda _, branch: branch == '[BRANCH NAME]'),
 )
