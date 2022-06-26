be2fea7bffbf4c8b23db9d3074ad428c495ccfc1
John Carroll <johncarroll@Johns-MBP.fios-router.home>
2022-06-25 21:44:46 -0400

    combined branches with deploy task

--- tasks.py
+++ tasks.py
@@ -76,6 +76,7 @@ DEPLOY_RULES = (
     ('stage', lambda _, branch: branch.startswith('release')),
     ('dev', lambda _, branch: branch == 'develop'),
     # Uncomment below and adjust branch name to deploy desired feature branch to the feature space
+    ('feature', lambda _, branch: branch == 'feature/jfc-and-title-display-for-feature'),
     # ('feature', lambda _, branch: branch == '[BRANCH NAME]'),
 )
 
