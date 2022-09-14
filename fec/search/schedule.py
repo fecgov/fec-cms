f93bed3fe817c3e12fe630fdf3d583523614e567
Priya Kasireddy <pkasireddy@fec.gov>
2022-09-14 15:58:46 -0400

    This is a test file to run simple cron job

--- fec/search/schedule.py
+++ fec/search/schedule.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 import os
 import logging
-import subprocess
-
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
@@ -11,5 +9,5 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 
 def run_job():
-    # logger.info('Hello, This is my first cron job...' + ' 2>&1 ')
-    # logger.info('Hello, This is my first cron job...' + os.path.join(BASE_DIR,'scheduler.log' + ' 2>&1 ')  
+    logger.info('Hello, This is my first cron job...' + ' 2>&1 ')
+    # logger.info('Hello, This is my first cron job...' + os.path.join(BASE_DIR,'scheduler.log' + ' 2>&1 ')
