5a2fc651f78b6c248b782691d099594b1dd2f901
R Fultz <rfultz@fec.gov>
2023-01-04 15:23:31 -0500

    No longer deploy to Feature, starting the hotfix branch

--- tasks.py
+++ tasks.py
@@ -77,7 +77,6 @@ DEPLOY_RULES = (
     ('dev', lambda _, branch: branch == 'develop'),
     # Uncomment below and adjust branch name to deploy desired feature branch to the feature space
     # ('feature', lambda _, branch: branch == '[BRANCH NAME]'),
-    ('feature', lambda _, branch: branch == 'feature/5507-admin-fines-calculator-2023'),
 )
 
 
