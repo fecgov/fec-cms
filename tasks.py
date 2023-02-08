b42869250258d45136d1a60c3d4a0053ea48ef88
John Carroll <johncarroll@Johns-MacBook-Pro.local>
2023-02-08 11:06:17 -0500

    remove task for dev deploy

--- tasks.py
+++ tasks.py
@@ -74,7 +74,7 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     ('stage', lambda _, branch: branch.startswith('release')),
-    ('dev', lambda _, branch: branch == 'feature/5261-sort-murs-by-case-no'),
+    ('dev', lambda _, branch: branch == 'develop'),
     # Uncomment below and adjust branch name to deploy desired feature branch to the feature space
     # ('feature', lambda _, branch: branch == '[BRANCH NAME]'),
 )
