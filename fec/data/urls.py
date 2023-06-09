8955d6484d900c057c32287dec1ba5d2fd2597a0
Robert <rfultz@fec.gov>
2023-06-09 13:24:19 -0400

    Debts - formatting

--- fec/data/urls.py
+++ fec/data/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     re_path(r'^data/candidate/(?P<candidate_id>\w+)/$', views.candidate),
     re_path(r'^data/committee/(?P<committee_id>\w+)/$', views.committee, name='committee-by-id'),
     re_path(r'^data/elections/(?P<office>\w+)/(?P<state>\w+)/(?P<district>\w+)/(?P<cycle>[0-9]+)/$',
-        views.elections, name='elections-house'),
+            views.elections, name='elections-house'),
     re_path(r'^data/elections/(?P<office>\w+)/(?P<state>\w+)/(?P<cycle>[0-9]+)/$', views.elections,
-        name='elections-senate'),
+            name='elections-senate'),
     re_path(r'^data/elections/president/(?P<cycle>[0-9]+)/$', views.elections_president, name='elections-president'),
     re_path(r'^data/elections/$', views.elections_lookup),
     re_path(r'^data/raising-bythenumbers/$', views.raising),
@@ -25,24 +25,24 @@ urlpatterns = [
 
     # Datatables
     re_path(r'^data/candidates/(?P<office>\w+)/$',
-        views_datatables.candidates_office),
+            views_datatables.candidates_office),
     re_path(r'^data/candidates/$', views_datatables.candidates),
     re_path(r'^data/committees/$', views_datatables.committees),
     re_path(r'^data/communication-costs/$',
-        views_datatables.communication_costs),
+            views_datatables.communication_costs),
     re_path(r'^data/disbursements/$', views_datatables.disbursements),
     re_path(r'^data/electioneering-communications/$',
-        views_datatables.electioneering_communications),
+            views_datatables.electioneering_communications),
     re_path(r'^data/filings/$', views_datatables.filings),
     re_path(r'^data/independent-expenditures/$',
-        views_datatables.independent_expenditures),
+            views_datatables.independent_expenditures),
     re_path(r'^data/individual-contributions/$',
-        views_datatables.individual_contributions),
+            views_datatables.individual_contributions),
     re_path(r'^data/loans/$', views_datatables.loans),
     re_path(r'^data/party-coordinated-expenditures/$',
-        views_datatables.party_coordinated_expenditures),
+            views_datatables.party_coordinated_expenditures),
     re_path(r'^data/receipts/individual-contributions/$',
-        views_datatables.individual_contributions),
+            views_datatables.individual_contributions),
     re_path(r'^data/receipts/$', views_datatables.receipts),
     re_path(r'^data/reports/(?P<form_type>[\w-]+)/$', views_datatables.reports),
     re_path(r'^legal-resources/enforcement/audit-search/$', views_datatables.audit),
@@ -56,7 +56,6 @@ if settings.FEATURES.get('pac_party'):
     )
 
 if settings.FEATURES.get('debts'):
-    # Debts section TODO: debts dates
     urlpatterns.append(
         re_path(r'^data/debts/$', views_datatables.debts)
     )
@@ -75,7 +74,7 @@ if settings.FEATURES.get('house_senate_overview'):
     """
     urlpatterns.append(
         re_path(r'^data/elections/(?P<office>\w+)/(?P<cycle>[0-9]+)/$', views.house_senate_overview,
-            name='elections-overview')
+                name='elections-overview')
     )
     urlpatterns.append(
         re_path(r'^data/elections/(?P<office>\w+)/$', views.house_senate_overview)
