0dc03d4cfe35f5a1ca922ca9b52931d405c2bfab
Robert <rfultz@fec.gov>
2023-09-01 07:58:54 -0400

    debts - deploy to Feature

--- tasks.py
+++ tasks.py
@@ -77,6 +77,7 @@ DEPLOY_RULES = (
     ('dev', lambda _, branch: branch == 'develop'),
     # Uncomment below and adjust branch name to deploy desired feature branch to the feature space
     # ('feature', lambda _, branch: branch == '[BRANCH NAME]'),
+    ('feature', lambda _, branch: branch == 'feature/5816 debts-line_number-filter'),
 )
 
 
