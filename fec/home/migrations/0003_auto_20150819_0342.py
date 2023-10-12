973fa8fac472ab5992d0119e453af0b888ff4189
Priya Kasireddy <pkasireddy@fec.gov>
2023-10-12 12:41:44 -0400

    Wagtail v5.0 and new migration file

--- fec/home/migrations/0003_auto_20150819_0342.py
+++ fec/home/migrations/0003_auto_20150819_0342.py
@@ -43,7 +43,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='homepage',
             name='body',
-            field=wagtail.fields.StreamField((('heading', wagtail.blocks.CharBlock(classname='full title')), ('paragraph', wagtail.blocks.RichTextBlock()), ('image', wagtail.images.blocks.ImageChooserBlock())), default=None),
+            field=wagtail.fields.StreamField((('heading', wagtail.blocks.CharBlock(classname='full title')), ('paragraph', wagtail.blocks.RichTextBlock()), ('image', wagtail.images.blocks.ImageChooserBlock())), default=None, null=True, blank=True),
             preserve_default=False,
         ),
     ]
