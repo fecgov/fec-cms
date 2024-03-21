9e55147cf6f438466005c80a282375729629a2ee
John Carroll <jcarroll@F612204M.fios-router.home>
2024-03-20 21:22:06 -0400

    rm feature deploy task

--- tasks.py
+++ tasks.py
@@ -76,7 +76,7 @@ DEPLOY_RULES = (
     ('stage', lambda _, branch: branch.startswith('release')),
     ('dev', lambda _, branch: branch == 'develop'),
     # Uncomment below and adjust branch name to deploy desired feature branch to the feature space
-    ('feature', lambda _, branch: branch == 'feature/6142-office_template-anchors-rich-text'),
+    # ('feature', lambda _, branch: branch == '[BRANCH NAME]'),
 )
 
 
