5de6d9e847a1bfdfaeecbc9b1f8ade7336e1b3b0
Pat Phongsvirajati <pphongsvirajati@fec.gov>
2024-04-16 12:44:35 -0400

    check list of IPs for matching X-Forwarded-For header value

--- fec/fec/context.py
+++ fec/fec/context.py
@@ -13,8 +13,11 @@ def api_key(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     # Default to public key
     api_key = settings.FEC_API_KEY_PUBLIC
-    # Check if X-Forwarded-For header matches internal IP
-    if x_forwarded_for == settings.FEC_INTERNAL_IP:
-        # Override API token with internal key
-        api_key = settings.FEC_INTERNAL_API_KEY_PUBLIC
+
+    # Update API_KEY based on X-Forwarded-For header value
+    if x_forwarded_for:
+        ip_list = [ip.strip() for ip in x_forwarded_for.split(',')]
+        if settings.FEC_INTERNAL_IP in ip_list:
+            api_key = settings.FEC_INTERNAL_API_KEY_PUBLIC
+
     return {'API_KEY_PUBLIC': api_key}
