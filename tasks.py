09ec0a036d9a4db5e16f3aa5e11e306519adbf81
Pat Phongsvirajati <pphongsvirajati@fec.gov>
2024-04-18 09:40:34 -0400

    deploy to feature

--- tasks.py
+++ tasks.py
@@ -79,7 +79,9 @@ DEPLOY_RULES = (
     ('dev', lambda _, branch: branch == 'develop'),
     # Uncomment below and adjust branch name to deploy desired feature branch
     # to the feature space
-    # ('feature', lambda _, branch: branch == '[BRANCH NAME]'),
+    ('feature', lambda _, branch:
+        branch == 'feature/test-regulation-' +
+        'search-using-ecfr-api'),
 )
 
 
