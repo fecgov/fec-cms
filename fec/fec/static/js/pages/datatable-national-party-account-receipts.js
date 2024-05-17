b6fb4e84f2945c62ecd5db6ba49e82b73dac4f54
Robert <rfultz@fec.gov>
2024-05-17 11:40:42 -0400

    NatPartyAcct - create receipts templates, filters

---
+++ fec/fec/static/js/pages/datatable-national-party-account-receipts.js
@@ -0,0 +1,27 @@
+/**
+ * National party account receipts datatable page
+ */
+
+var $ = require('jquery');
+
+var tables = require('../modules/tables');
+var columns = require('../modules/columns');
+
+var detailsPanelTemplate = require('../templates/national-party-account-receipts.hbs');
+
+$(function() {
+  var $table = $('#results');
+  new tables.DataTable($table, {
+    autoWidth: false,
+    title: 'National party account receipts',
+    path: ['national_party', 'schedule_a'],
+    columns: columns.nationalPartyReceipts,
+    order: [[4, 'desc']],
+    useFilters: true,
+    useExport: true,
+    rowCallback: tables.modalRenderRow,
+    callbacks: {
+      afterRender: tables.modalRenderFactory(detailsPanelTemplate)
+    }
+  });
+});
