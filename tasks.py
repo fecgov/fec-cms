f10fe41dd4c088127ffceaafca09fa505f6aa6b7
pkfec <pkasireddy@fec.gov>
2024-08-20 12:08:33 -0400

    bump setuptools to latest v73.0.1

--- tasks.py
+++ tasks.py
@@ -73,7 +73,7 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     ('stage', lambda _, branch: branch.startswith('release')),
-    ('dev', lambda _, branch: branch == 'feature/6400-remove-slacker'),
+    ('dev', lambda _, branch: branch == 'develop'),
     # Uncomment below and adjust branch name to deploy desired feature branch to the feature space
     # ('feature', lambda _, branch: branch == '[BRANCH NAME]'),
 )
