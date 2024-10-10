f1b500e531961ce722aad3d9e14af55ceb09a257
Robert <rfultz@fec.gov>
2024-10-10 12:39:30 -0400

    AO filters - deploy to Feature

--- tasks.py
+++ tasks.py
@@ -76,6 +76,7 @@ DEPLOY_RULES = (
     ('dev', lambda _, branch: branch == 'develop'),
     # Uncomment below and adjust branch name to deploy desired feature branch to the feature space
     # ('feature', lambda _, branch: branch == '[BRANCH NAME]'),
+    ('feature', lambda _, branch: branch == 'feature/6443-update-ao-search-templates'),
 )
 
 
