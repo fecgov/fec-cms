b6a8c2d62a74e406687d2954158669cca88d5f72
John Carroll <jcarroll@Mac.fios-router.home>
2025-06-23 10:02:03 -0400

    rm temp feature deploy task

--- tasks.py
+++ tasks.py
@@ -75,7 +75,7 @@ DEPLOY_RULES = (
     ('stage', lambda _, branch: branch.startswith('release')),
     ('dev', lambda _, branch: branch == 'develop'),
     # Uncomment below and adjust branch name to deploy desired feature branch to the feature space
-    ('feature', lambda _, branch: branch == 'feature/6670-legal-proximity-keyword-search'),
+    ('feature', lambda _, branch: branch == '[BRANCH NAME]'),
 )
 
 
