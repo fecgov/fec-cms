8e6a2d33f775370800c7d1df95ea63ea110ece2e
Robert <rfultz@fec.gov>
2025-09-15 15:15:05 -0400

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -73,8 +73,7 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     ('stage', lambda _, branch: branch.startswith('release')),
-    # ('dev', lambda _, branch: branch == 'develop'),
-    ('dev', lambda _, branch: branch == 'feature/6711-fecfile-eligibility'),
+    ('dev', lambda _, branch: branch == 'develop'),
     # Uncomment below and adjust branch name to deploy desired feature branch to the feature space
     # ('feature', lambda _, branch: branch == '[BRANCH NAME]'),
     ('feature', lambda _, branch: branch == 'feature/6711-fecfile-eligibility'),  # TEMP
