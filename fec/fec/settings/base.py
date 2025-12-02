7fa0790f0250bbe5978fbafa04a89ea698698f02
Robert <rfultz@fec.gov>
2025-12-02 15:22:46 -0500

    Rulemakings - tighten X_FRAME

--- fec/fec/settings/base.py
+++ fec/fec/settings/base.py
@@ -390,8 +390,3 @@ LOGGING = {
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-
-# For the rulemakings' single page PDF viewer
-# TODO: can we set this for only PDFs loading into only rulemaking single page <object> and <iframe>?
-if FEATURES['rulemakings']:
-    X_FRAME_OPTIONS = "SAMEORIGIN"
