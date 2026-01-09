46dd1fd3aa223cea21a1f448f19d6211929aacc4
John Carroll <jcarroll@Mac.fios-router.home>
2026-01-09 02:19:06 -0500

    q_exclude,proximity,doc display by doc-then its L2 children,dont show dup docs

--- fec/fec/static/js/pages/datatable-rulemakings.js
+++ fec/fec/static/js/pages/datatable-rulemakings.js
@@ -70,43 +70,51 @@ KeywordProximityFilter.prototype.handleNumberChange = function(e) {
 $('#search-input').on('change', function(e) {
   handleKeywordSearchChange(e);
  });
-
+  let new_val;
   const handleKeywordSearchChange = function(e) {
-  const newVal = e.target.value;
+  new_val = e.target.value;
   const currentTag = document.querySelectorAll('.tags .tag__item[data-id="search-input"]');
   // If there's already a tag, we need to change its label
   if (currentTag.length >= 1) {
     currentTag.forEach((tag, i) => {
       // We only want to keep one filter tag for keywords, so change its label
       // but only if it has a value to show
-      if (i === 0 && newVal.length > 0)
-        $(tag).contents()[0].nodeValue = newVal;
+      if (i === 0 && new_val.length > 0)
+        $(tag).contents()[0].nodeValue = new_val;
       // Otherwise, if it's after the first one, click its X button
       else tag.querySelector('.js-close').click();
     });
   }
   else {
-    $('.tags').attr('aria-hidden', 'false').append(`<li data-tag-category="q" class="tag__category"><div data-id="search-input" data-removable="true" class="tag__item">${newVal}
+    $('.tags').attr('aria-hidden', 'false').append(`<li data-tag-category="q" class="tag__category"><div data-id="search-input" data-removable="true" class="tag__item">${new_val}
     <button class="button js-close tag__remove"><span class="u-visually-hidden">Remove</span></button>
     </div></li>`);
   }
 
-   let query;
-   if (newVal != '') {
-    query = URI(window.location.search)
-    .removeSearch('q')
-    .addSearch('q', newVal);
-   }
-   else {
-    query = URI(window.location.search)
-     .removeSearch('q');
-   }
+  const new_queryParams = new URLSearchParams(window.location.search);
+  const q_params = new_val;
 
-  window.history.pushState(
-      null,
-      '',
-      window.location.pathname + query.toString()
-      );
+      // We need to divide any 'search' value into q and q_exclude, split on ` -`
+      // And put q value into q field and q_excluse value into hidden q_exclude field
+      const qStrings = [];
+      const qExcludeStrings = [];
+      if (q_params.indexOf(' -') >= 0) {
+        const allTerms = q_params.split(' ');
+        allTerms.forEach(term => {
+          if (term.startsWith('-'))
+            qExcludeStrings.push(term.substring(1));
+          else qStrings.push(term);
+        });
+        if (qStrings.length > 0)
+          new_queryParams['q'] = qStrings.join(' ');
+          $('input[name="q"]').val(new_queryParams.q);
+        if (qExcludeStrings.length > 0)
+          new_queryParams['q_exclude'] = qExcludeStrings.join(' ');
+          $('input[name="q_exclude"]').val(new_queryParams.q_exclude);
+
+      } else {
+        new_queryParams['q'] = q_params;
+      }
 };
 
 // Remove max-gaps value from field and querystring upon tag removal
@@ -137,18 +145,47 @@ $(document).on('click', '.js-close.tag__remove[data-filter-id="keyword-proximity
     title: 'Rulemakings',
     path: ['rulemaking', 'search'],
     columns: cols_rulemakings,
-    order: [[0, 'desc']],
+    order: [[2, 'desc']],
     useFilters: true,
     useExport: false,
     // Initiate the field value and fire change for keyword if included in querystring in a link or copy/pasted url
-    // TODO: DO I NEED THIS AT ALL?, ALSO - If so Don't think I need to also add tags here(commented out)...ends up with two tags once I added trigger('change')
-    initComplete: function () {
-       const queryParams = URI.parseQuery(window.location.search);
-       //const queryParams = new URLSearchParams(window.location.search);
-      if (queryParams.q) {
-        $('input[name="q"]').val(queryParams.q).trigger('change');
-      }
-    }
+    // TODO: This ^^ is currently not working correctly--retrurns 'undefined' for q value
+    // initComplete: function () {
+    //    const queryParams = URI.parseQuery(window.location.search);
+    //   if (queryParams.q) {
+    //     console.log('XX:', queryParams)
+    //     $('input[name="q"]').val(queryParams.q).trigger('change');
+    //   }
+    // },
+
+    // After separating q and q_exclude, and getting reults from API call, put the original keyword search query
+    // back into keyword (q)field to preserve any frontend keyword search operator syntax should a user want to edit
+    // the value directly in the field instead of using the modal form to generate ta new query.
+    drawCallback: function () {
+         $('input[name="q"]').val(new_val);
+         $('input[name="q_exclude"]').val('');
+         //TODO: May just need 'if (new_val) {...'
+            const newer_queryParams = URI.parseQuery(window.location.search);
+            if (newer_queryParams.q) {
+              let query;
+              if (new_val != '') {
+                query = URI(window.location.search)
+                .removeSearch('q')
+                .removeSearch('q_exclude')//, `${newer_queryParams['q_exclude']}`)
+                .addSearch('q', `${new_val}`);
+
+              }
+              else {
+                query = URI(window.location.search)
+                .removeSearch('q');
+              }
+              window.history.pushState(
+                  null,
+                  '',
+                  window.location.pathname + query.toString()
+                );
+            }
+          }
   });
 
 });
